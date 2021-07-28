1f51726a8adab5289e807f8a93100f96875bce76
Emily Coco <emilycoco@10-22-18-26.dynapool.wireless.nyu.edu>
2021-07-28 11:29:09 -0400

    fixing get dataframe script

--- analysis/ExtendedModel-analysis/get-all-data.R
+++ analysis/ExtendedModel-analysis/get-all-data.R
@@ -1,19 +1,19 @@
 library(tidyverse)
 
-data = list()
-index = 1
-
+# data = list()
+# index = 1
+# 
 #for reading on HPC or local paths
-for(d in list.dirs(path = "../output")) {
-  files = list.files(path = dirs[d], full.names = T)
-  for(f in files)  {
-    if(!is.null(f)) {
-      data[[index]] = read.csv(f)
-      index = index + 1
-    }
-  }
-}
-alldata = bind_rows(data)
+# for(d in list.dirs(path = "../output")) {
+#   files = list.files(path = dirs[d], full.names = T)
+#   for(f in files)  {
+#     if(!is.null(f)) {
+#       data[[index]] = read.csv(f)
+#       index = index + 1
+#     }
+#   }
+# }
+# alldata = bind_rows(data)
 
 
 alldata = data.frame()
@@ -25,10 +25,9 @@ for(d in list.dirs(path = "../output")) {
     }
   }
 }
-
-
 save(alldata, file = "alldata.RData")
 
+parameters = colnames(alldata[,c(1,4:9,11:17)])
 tavg = alldata %>% group_by_at(unlist(c(parameters, "exp", "model_year"))) %>%
   summarize(cr = mean(cortex.ratio, na.rm = T), 
             ri = mean(recycling.intensity, na.rm = T), 
