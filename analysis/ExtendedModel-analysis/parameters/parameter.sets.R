717c2d0510c0854553be30b57ebc4a95c59d401c
cocoemily <ec3307@nyu.edu>
2021-08-05 11:52:57 -0400

    parameter sets that are still missing due to heap errors

--- analysis/ExtendedModel-analysis/parameters/parameter.sets.R
+++ analysis/ExtendedModel-analysis/parameters/parameter.sets.R
@@ -147,3 +147,23 @@ setwd("..")
 write.csv(mparams, file = "run-scripts/ExtendedModel/mparams.csv", row.names = F)
 setwd("analysis") #move back to analysis folder
 
+
+#due to problems with the heap memory size
+stillmissing = c(33186, 47554, 47558, 47560, 47562, 47564, 47571, 47574, 
+                 47577, 47580, 47599, 47616, 89014, 89019, 89021, 102529, 
+                 102532, 102534, 102537, 102541, 102545, 102546, 102549, 
+                 102551, 102553, 102558, 102560, 102628, 102632, 102634, 
+                 102654, 102656, 102728, 102729, 102732, 102735, 102736, 
+                 102739, 102740, 102741, 102746, 102751, 102752, 102817, 
+                 102818, 102819, 102820’, 102821, 102822, 102823, 102824, 
+                 102825, 102826, 102827, 102828, 102829, 102830, 102831, 
+                 102832, 102833, 102834, 102835, 102836, 102837, 102838, 
+                 102839, 102840, 102841, 102842, 102843, 102844, 102845, 
+                 102846, 102847, 102848)
+
+mparams2 = clean.parameters %>%
+  filter(of %in% stillmissing)
+
+setwd("..")
+write.csv(mparams2, file = "run-scripts/ExtendedModel/mparams2.csv", row.names = F)
+setwd("analysis") #move back to analysis folder
