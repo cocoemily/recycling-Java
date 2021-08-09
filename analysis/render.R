a05f5a48488711ff6d25a025905b5ead6507a9bc
cocoemily <ec3307@nyu.edu>
2021-08-09 14:05:00 -0400

    adding in three way interaction AOVs

--- analysis/render.R
+++ analysis/render.R
@@ -1,4 +1,5 @@
-library("rmarkdown")
+library(rmarkdown)
+library(tidyverse)
 
 alldata = read_csv(file = "output/joined_data-small.csv")
 rmarkdown::render("parameter-interaction.Rmd", output_file = "small-size_parameter-interaction.html")
