2b973952a32a03b06caa325ed4f02d9f46dbd968
cocoemily <ec3307@nyu.edu>
2021-08-26 11:31:04 -0400

    interaction Rmd for small world model runs

--- analysis/render.R
+++ analysis/render.R
@@ -1,9 +1,11 @@
 library(rmarkdown)
 library(tidyverse)
 
+setwd("..")
 alldata = read_csv(file = "output/joined_data-small.csv")
+setwd("analysis")
 rmarkdown::render("parameter-interaction.Rmd", output_file = "small-size_parameter-interaction.html")
 
 
-alldata = read_csv(file = "output/joined_data-large.csv")
-rmarkdown::render("parameter-interaction.Rmd", output_file = "large-size_parameter-interaction.html")
+#alldata = read_csv(file = "output/joined_data-large.csv")
+#rmarkdown::render("parameter-interaction.Rmd", output_file = "large-size_parameter-interaction.html")
