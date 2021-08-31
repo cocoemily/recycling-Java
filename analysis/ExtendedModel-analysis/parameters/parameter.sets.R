3bb6675c31a47b7cb9947ab23f34053590d613c1
cocoemily <ec3307@nyu.edu>
2021-08-31 11:11:00 -0400

    adding tukey comparisons to parameter analysis rmd

--- analysis/ExtendedModel-analysis/parameters/parameter.sets.R
+++ analysis/ExtendedModel-analysis/parameters/parameter.sets.R
@@ -88,11 +88,6 @@ save(parameters, file = "ExtendedModel-analysis/parameters/parameters.RData")
 options(scipen=10)
 parameters[,c(2:13, 16, 17, 19:ncol(parameters))] = lapply(parameters[,c(2:13, 16, 17, 19:ncol(parameters))], function(x) as.numeric(as.character(x)))
 
-setwd("..") #moves up a directory
-write.csv(parameters, file = "run-scripts/parameters.csv", row.names=F)
-write.csv(parameters[1:10,], file = "run-scripts/test.csv", row.names=F)
-setwd("analysis") #move back to analysis folder
-
 
 #need different parameter files to run on HPC
 paramcsv = list()
@@ -120,62 +115,62 @@ for(i in 21:40) {
 
 setwd("..")
 for(i in 1:length(paramcsv2)) {
-  filename = paste0("run-scripts/ExtendedModel/params", i, ".csv")
+  filename = paste0("run-scripts/ExtendedModel-sensitivity/params", i, ".csv")
   write.csv(paramcsv2[[i]], file = filename, row.names=F)
 }
 setwd("analysis") #move back to analysis folder
 
 
-##get csv with missing parameters from previous model runs
-missing = c(5533, 16598, 33168, 47305, 47308, 47312, 47457, 47503, 
-            47515, 47554, 47555, 47558, 47560, 47561, 47562, 47563, 
-            47564, 47570, 47571, 47574, 47576, 47577, 47578, 47579, 
-            47580, 47581, 47587, 47589, 47590, 47594, 47596, 47599, 
-            47601, 47605, 47607, 47608, 47611, 47612, 47616, 88709, 
-            88995, 88996, 89003, 89014, 89016, 89019, 89020, 89021, 
-            89023, 102529, 102532, 102534, 102537, 102541, 102543, 102545, 
-            102546, 102548, 102549, 102551, 102553, 102554, 102556, 102557, 
-            102558, 102560, 102625, 102627, 102628, 102632, 102634, 102639, 
-            102640, 102641, 102645, 102649, 102652, 102654, 102655, 102656, 
-            102724, 102726, 102728, 102729, 102732, 102733, 102735, 102736, 
-            102739, 102740, 102741, 102743, 102744, 102745, 102746, 102748, 
-            102751, 102752, 102817, 102818, 102819, 102820, 102821, 102822, 
-            102823, 102824, 102825, 102826, 102827, 102828, 102829, 102830, 
-            102831, 102832, 102833, 102834, 102835, 102836, 102837, 102838, 
-            102839, 102840, 102841, 102842, 102843, 102844, 102845, 102846, 
-            102847, 102848, 102868)
-mparams = parameters %>%
-  filter(of %in% missing)
-
-setwd("..")
-write.csv(mparams, file = "run-scripts/ExtendedModel/mparams.csv", row.names = F)
-setwd("analysis") #move back to analysis folder
-
-
-#due to problems with the heap memory size
-stillmissing = c(33186, 47554, 47558, 47560, 47562, 47564, 47571, 47574, 
-                 47577, 47580, 47599, 47616, 89014, 89019, 89021, 102529, 
-                 102532, 102534, 102537, 102541, 102545, 102546, 102549, 
-                 102551, 102553, 102558, 102560, 102628, 102632, 102634, 
-                 102654, 102656, 102728, 102729, 102732, 102735, 102736, 
-                 102739, 102740, 102741, 102746, 102751, 102752, 102817, 
-                 102818, 102819, 102820, 102821, 102822, 102823, 102824, 
-                 102825, 102826, 102827, 102828, 102829, 102830, 102831, 
-                 102832, 102833, 102834, 102835, 102836, 102837, 102838, 
-                 102839, 102840, 102841, 102842, 102843, 102844, 102845, 
-                 102846, 102847, 102848)
-
-mparams2 = parameters %>%
-  filter(of %in% stillmissing)
-
-setwd("..")
-write.csv(mparams2, file = "run-scripts/ExtendedModel/mparams2.csv", row.names = F)
-setwd("analysis") #move back to analysis folder
-
-
-test = mparams2 %>% filter(of %in% c(47554, 47558, 47560, 47562)) %>%
-  mutate(of = c("test1", "test2", "test3", "test4"))
-
-setwd("..")
-write.csv(test, file = "run-scripts/ExtendedModel/test.csv", row.names = F)
-setwd("analysis") #move back to analysis folder
+# ##get csv with missing parameters from previous model runs
+# missing = c(5533, 16598, 33168, 47305, 47308, 47312, 47457, 47503, 
+#             47515, 47554, 47555, 47558, 47560, 47561, 47562, 47563, 
+#             47564, 47570, 47571, 47574, 47576, 47577, 47578, 47579, 
+#             47580, 47581, 47587, 47589, 47590, 47594, 47596, 47599, 
+#             47601, 47605, 47607, 47608, 47611, 47612, 47616, 88709, 
+#             88995, 88996, 89003, 89014, 89016, 89019, 89020, 89021, 
+#             89023, 102529, 102532, 102534, 102537, 102541, 102543, 102545, 
+#             102546, 102548, 102549, 102551, 102553, 102554, 102556, 102557, 
+#             102558, 102560, 102625, 102627, 102628, 102632, 102634, 102639, 
+#             102640, 102641, 102645, 102649, 102652, 102654, 102655, 102656, 
+#             102724, 102726, 102728, 102729, 102732, 102733, 102735, 102736, 
+#             102739, 102740, 102741, 102743, 102744, 102745, 102746, 102748, 
+#             102751, 102752, 102817, 102818, 102819, 102820, 102821, 102822, 
+#             102823, 102824, 102825, 102826, 102827, 102828, 102829, 102830, 
+#             102831, 102832, 102833, 102834, 102835, 102836, 102837, 102838, 
+#             102839, 102840, 102841, 102842, 102843, 102844, 102845, 102846, 
+#             102847, 102848, 102868)
+# mparams = parameters %>%
+#   filter(of %in% missing)
+# 
+# setwd("..")
+# write.csv(mparams, file = "run-scripts/ExtendedModel/mparams.csv", row.names = F)
+# setwd("analysis") #move back to analysis folder
+# 
+# 
+# #due to problems with the heap memory size
+# stillmissing = c(33186, 47554, 47558, 47560, 47562, 47564, 47571, 47574, 
+#                  47577, 47580, 47599, 47616, 89014, 89019, 89021, 102529, 
+#                  102532, 102534, 102537, 102541, 102545, 102546, 102549, 
+#                  102551, 102553, 102558, 102560, 102628, 102632, 102634, 
+#                  102654, 102656, 102728, 102729, 102732, 102735, 102736, 
+#                  102739, 102740, 102741, 102746, 102751, 102752, 102817, 
+#                  102818, 102819, 102820, 102821, 102822, 102823, 102824, 
+#                  102825, 102826, 102827, 102828, 102829, 102830, 102831, 
+#                  102832, 102833, 102834, 102835, 102836, 102837, 102838, 
+#                  102839, 102840, 102841, 102842, 102843, 102844, 102845, 
+#                  102846, 102847, 102848)
+# 
+# mparams2 = parameters %>%
+#   filter(of %in% stillmissing)
+# 
+# setwd("..")
+# write.csv(mparams2, file = "run-scripts/ExtendedModel/mparams2.csv", row.names = F)
+# setwd("analysis") #move back to analysis folder
+# 
+# 
+# test = mparams2 %>% filter(of %in% c(47554, 47558, 47560, 47562)) %>%
+#   mutate(of = c("test1", "test2", "test3", "test4"))
+# 
+# setwd("..")
+# write.csv(test, file = "run-scripts/ExtendedModel/test.csv", row.names = F)
+# setwd("analysis") #move back to analysis folder
