182e3449f65ec068bfc9dc9ebfc16c5ba6d78bf5
cocoemily <ec3307@nyu.edu>
2021-09-02 10:06:24 -0400

    parameter sensitivity with pairwise comparisons

--- analysis/render.R
+++ analysis/render.R
@@ -6,6 +6,10 @@ alldata = read_csv(file = "output/joined_data-small.csv")
 setwd("analysis")
 rmarkdown::render("parameter-interaction.Rmd", output_file = "small-size_parameter-interaction.html")
 
+#alldata = read_csv(file = "output/joined_data-small.csv")
+#alldata = alldata %>% filter(model_year < 394000)
+#alldata = alldata %>% filter(!is.na(total.RI))
+#setwd("analysis")
+#rmarkdown::render("parameter-interaction.Rmd", output_file = "small-size_minus-initial-time.html")
 
-#alldata = read_csv(file = "output/joined_data-large.csv")
-#rmarkdown::render("parameter-interaction.Rmd", output_file = "large-size_parameter-interaction.html")
+
