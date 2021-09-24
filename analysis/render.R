3707158b699be15d04754e0139076f52d33694de
cocoemily <ec3307@nyu.edu>
2021-09-24 13:51:17 -0400

    creating preliminary analysis scripts for model experiments

--- analysis/render.R
+++ analysis/render.R
@@ -2,9 +2,9 @@ library(rmarkdown)
 library(tidyverse)
 
 setwd("..")
-alldata = read_csv(file = "output/joined_data-small.csv")
-setwd("analysis")
-rmarkdown::render("parameter-interaction.Rmd", output_file = "small-size_parameter-interaction.html")
+#alldata = read_csv(file = "output/joined_data-small.csv")
+#setwd("analysis")
+#rmarkdown::render("parameter-interaction.Rmd", output_file = "small-size_parameter-interaction.html")
 
 #alldata = read_csv(file = "output/joined_data-small.csv")
 #alldata = alldata %>% filter(model_year < 394000)
@@ -13,3 +13,6 @@ rmarkdown::render("parameter-interaction.Rmd", output_file = "small-size_paramet
 #rmarkdown::render("parameter-interaction.Rmd", output_file = "small-size_minus-initial-time.html")
 
 
+alldata = read_csv(file = "output/joined_model_data.csv")
+setwd("analysis")
+rmarkdown::render("experiments-with-recycling.Rmd", output_file = "small-size_parameter-interaction.html")
