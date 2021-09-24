1f0df15900f0e6d0957e6f53184c97890b834d5d
cocoemily <ec3307@nyu.edu>
2021-09-24 15:16:51 -0400

    subsetting dataset read in

--- analysis/render.R
+++ analysis/render.R
@@ -2,13 +2,18 @@ library(rmarkdown)
 library(tidyverse)
 library(data.table)
 
+parameters = c("max_use_intensity", "max_artifact_carry", "max_flake_size", 
+               "max_nodules_size", "blank_prob", "scavenge_prob", "overlap", 
+               "mu", "size_preference", "flake_preference", 
+               "minx_suitable_flake_size", "strict_selection")
+
 setwd("..")
 #alldata = as.data.frame(fread("output/joined_data-small.csv"))
 #setwd("analysis")
 #rmarkdown::render("parameter-interaction.Rmd", output_file = "small-size_parameter-interaction.html")
 
 
-alldata = as.data.frame(fread("output/joined_model_data.csv"))
+alldata = as.data.frame(fread("output/joined_model_data.csv", select=c(parameters, "model_year", "total.RI")))
 setwd("analysis")
 rmarkdown::render("experiments-with-recycling.Rmd", output_file = 
 "missing-recycling.html")
