09b296b4fd40fede4199ca7e0516bf42e5816efa
cocoemily <ec3307@nyu.edu>
2021-09-24 15:12:20 -0400

    update to render Rscript

--- analysis/render.R
+++ analysis/render.R
@@ -1,18 +1,14 @@
 library(rmarkdown)
 library(tidyverse)
+library(data.table)
 
 setwd("..")
-#alldata = read_csv(file = "output/joined_data-small.csv")
+#alldata = as.data.frame(fread("output/joined_data-small.csv"))
 #setwd("analysis")
 #rmarkdown::render("parameter-interaction.Rmd", output_file = "small-size_parameter-interaction.html")
 
-#alldata = read_csv(file = "output/joined_data-small.csv")
-#alldata = alldata %>% filter(model_year < 394000)
-#alldata = alldata %>% filter(!is.na(total.RI))
-#setwd("analysis")
-#rmarkdown::render("parameter-interaction.Rmd", output_file = "small-size_minus-initial-time.html")
 
-
-alldata = read_csv(file = "output/joined_model_data.csv")
+alldata = as.data.frame(fread("output/joined_model_data.csv"))
 setwd("analysis")
-rmarkdown::render("experiments-with-recycling.Rmd", output_file = "small-size_parameter-interaction.html")
+rmarkdown::render("experiments-with-recycling.Rmd", output_file = 
+"missing-recycling.html")
