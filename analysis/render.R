e600c98e8b06ffa6c342ba230e3ea98f1a348269
cocoemily <ec3307@nyu.edu>
2021-10-05 10:45:32 -0400

    attempting different render method

--- analysis/render.R
+++ analysis/render.R
@@ -1,6 +1,7 @@
 library(rmarkdown)
 library(tidyverse)
 library(data.table)
+library(knitr)
 
 parameters = c("max_use_intensity", "max_artifact_carry", "max_flake_size", 
                "max_nodules_size", "blank_prob", "scavenge_prob", "overlap", 
@@ -10,8 +11,8 @@ parameters = c("max_use_intensity", "max_artifact_carry", "max_flake_size",
 setwd("..")
 alldata = as.data.frame(fread("output/joined_sensitivity-data.csv"))
 setwd("analysis")
-rmarkdown::render("parameter-interaction.Rmd", output_file = "sensitivity-analysis.html")
-
+#rmarkdown::render("parameter-interaction.Rmd", output_file = "sensitivity-analysis.html")
+knitr::knit2html("parameter-interaction.Rmd", output = "sensitivity-analysis.html")
 
 #alldata = as.data.frame(fread("output/joined_model_data.csv"))
 #setwd("analysis")
