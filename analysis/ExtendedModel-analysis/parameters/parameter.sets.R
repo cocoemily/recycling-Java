47b84bc492b797ccea5ba8f8e20bd725c6ffb2fe
cocoemily <ec3307@nyu.edu>
2021-10-07 13:08:37 -0400

    reducing number of model steps to 3000 for sensitivity analysis

--- analysis/ExtendedModel-analysis/parameters/parameter.sets.R
+++ analysis/ExtendedModel-analysis/parameters/parameter.sets.R
@@ -60,7 +60,7 @@ for(s in size.values) {
                               st, #strict
                               0.5, #ED 
                               0, #GF
-                              5000 #totalSteps 
+                              3000 #totalSteps 
                             )
                             parameters = rbind(parameters, newrow)
                             exp.num = exp.num + 1
