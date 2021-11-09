40750095f0f90fb0c0141a2e12b4a5981666d845
cocoemily <ec3307@nyu.edu>
2021-11-09 09:29:08 +0600

    adding tests for parallel processing with beta regs

--- analysis/test-beta-reg.R
+++ analysis/test-beta-reg.R
@@ -41,21 +41,32 @@ alldata = alldata %>% filter(size != "size") %>%
 u_alldata = alldata %>% mutate(s.total.RI = ifelse(total.RI == 0, (total.RI + 0.0001), total.RI)) %>% mutate(s.total.RI = ifelse(s.total.RI == 1, (s.total.RI - 0.0001), s.total.RI))
 obreg = betareg(s.total.RI ~ model_year, data = u_alldata)
 
-start.time <- Sys.time()
-breg_msns = betareg(s.total.RI ~ model_year + min_suitable_nodule_size, data = u_alldata)
-end.time <- Sys.time()
-time.taken <- end.time - start.time
-print(time.taken)
+# start.time <- Sys.time()
+# breg_msns = betareg(s.total.RI ~ model_year + min_suitable_nodule_size, data = u_alldata)
+# end.time <- Sys.time()
+# time.taken <- end.time - start.time
+# print(time.taken)
+# 
+# start.time <- Sys.time()
+# lrtest(breg_msns, obreg)
+# end.time <- Sys.time()
+# time.taken <- end.time - start.time
+# print(time.taken)
+# 
+# start.time <- Sys.time()
+# coeftest(breg_msns, vcov = vcovBS(breg_msns, R=25))
+# end.time <- Sys.time()
+# time.taken <- end.time - start.time
+# print(time.taken)
 
-start.time <- Sys.time()
-lrtest(breg_msns, obreg)
-end.time <- Sys.time()
-time.taken <- end.time - start.time
-print(time.taken)
+#test parallel processing
+beta_regression = function(variable) {
+  modelfun = paste("s.total.RI ~ model_year + ", variable)
+  breg = betareg(eval(parse(text = modelfun)), data = u_alldata)
+  lrtest(breg, obreg)
+  #coeftest(breg, vcov = vcovBS(breg, R=25))
+}
 
-start.time <- Sys.time()
-coeftest(breg_msns, vcov = vcovBS(breg_msns, R=25))
-end.time <- Sys.time()
-time.taken <- end.time - start.time
-print(time.taken)
+mclapply(c("blank_prob", "scavenge_prob"), beta_regression, mc.cores=2)
+#mclapply(parameters, beta_regression, mc.cores = 13)
 
