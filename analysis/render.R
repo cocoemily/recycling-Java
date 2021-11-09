45af0cf78b6a1e9a18aef9f5995a24370d3d14b8
cocoemily <ec3307@nyu.edu>
2021-11-09 13:30:48 +0600

    finalizing output for looking at parameter interactions graphically

--- analysis/render.R
+++ analysis/render.R
@@ -11,10 +11,6 @@ parameters = c("max_use_intensity", "max_artifact_carry", "max_flake_size",
 setwd("..")
 alldata = read_csv("output/joined_sensitivity-data.csv")
 setwd("analysis")
-#rmarkdown::render("parameter-interaction.Rmd", output_file = "sensitivity-analysis.html")
-rmarkdown::render("sensitivity-regressions.Rmd", output_file = "sensitivity-regressions.html")
-#rmarkdown::render("sensitivity-regressions.Rmd", output_file = "sensitivity-indiv_regressions_par-test.html")
+rmarkdown::render("parameter-interaction.Rmd", output_file = "sensitivity-analysis-interactions.html")
+#rmarkdown::render("sensitivity-regressions.Rmd", output_file = "sensitivity-regressions.html")
 
-#alldata = as.data.frame(fread("output/joined_model_data.csv"))
-#setwd("analysis")
-#rmarkdown::render("experiments-with-recycling.Rmd", output_file = "missing-recycling.html")
