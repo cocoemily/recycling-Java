a67c37a3d161c21dbd851bba43a5d517fa157e78
cocoemily <ec3307@nyu.edu>
2021-11-11 10:22:00 +0600

    adding in analysis of coef significance

--- analysis/full-beta-models.R
+++ analysis/full-beta-models.R
@@ -18,7 +18,9 @@ u_alldata = alldata %>% mutate(s.total.RI = ifelse(total.RI == 0, (total.RI + 0.
 
 #full model beta regression analysis
 breg1 = betareg(s.total.RI ~ model_year + max_use_intensity  + max_artifact_carry + max_flake_size + max_nodules_size + blank_prob + scavenge_prob + overlap + mu + size_preference + flake_preference + min_suitable_flake_size + min_suitable_nodule_size + strict_selection , data=u_alldata)
+coeftest(breg1)
 
 breg2 = betareg(s.total.RI ~ model_year + max_use_intensity  + max_artifact_carry + max_flake_size + max_nodules_size + blank_prob + scavenge_prob + overlap + mu + size_preference + flake_preference + min_suitable_flake_size + strict_selection , data=u_alldata)
+coeftest(breg2)
 
 lrtest(breg1, breg2)
