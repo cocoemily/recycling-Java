ce910066df58e7e4bca1adfeee78cd72b755d4db
cocoemily <ec3307@nyu.edu>
2021-11-10 23:31:00 -0500

    cleaning up hpc, adding regression outputs

--- analysis/render.R
+++ analysis/render.R
@@ -11,6 +11,6 @@ parameters = c("max_use_intensity", "max_artifact_carry", "max_flake_size",
 setwd("..")
 alldata = read_csv("output/joined_sensitivity-data.csv")
 setwd("analysis")
-rmarkdown::render("parameter-interaction.Rmd", output_file = "sensitivity-analysis-interactions.html")
-#rmarkdown::render("sensitivity-regressions.Rmd", output_file = "sensitivity-regressions.html")
+#rmarkdown::render("parameter-interaction.Rmd", output_file = "sensitivity-analysis-interactions.html")
+rmarkdown::render("sensitivity-regressions.Rmd", output_file = "sensitivity-regressions.html")
 
