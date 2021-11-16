8f0aba9f50148048f4e1783f3b49931b601115e6
cocoemily <ec3307@nyu.edu>
2021-11-15 21:25:21 -0500

    regressions for sensitivity analysis

--- analysis/ExtendedModel-analysis/sensitivity-analysis/test-beta-reg.R
+++ analysis/ExtendedModel-analysis/sensitivity-analysis/test-beta-reg.R
@@ -5,7 +5,8 @@ library(betareg)
 
 args = commandArgs(trailingOnly=TRUE)
 parameters = c("max_use_intensity", "max_artifact_carry", "max_flake_size","max_nodules_size", "blank_prob", "scavenge_prob", "overlap","mu", "size_preference", "flake_preference","min_suitable_flake_size", "min_suitable_nodule_size", "strict_selection")
-
+print(args[1])
+print(parameters[as.numeric(args[1])])
 
 alldata = read_csv("/scratch/ec3307/recycling-Java/output/joined_sensitivity-data.csv")
 
@@ -17,7 +18,7 @@ alldata = alldata %>% filter(size != "size") %>%
   filter(!is.na(total.RI))
 
 u_alldata = alldata %>% mutate(s.total.RI = ifelse(total.RI == 0, (total.RI + 0.0001), total.RI)) %>% mutate(s.total.RI = ifelse(s.total.RI == 1, (s.total.RI - 0.0001), s.total.RI))
-obreg = betareg(s.total.RI ~ model_year, data = u_alldata)
+#obreg = betareg(s.total.RI ~ model_year, data = u_alldata)
 
 #beta regression function
 beta_regression = function(variable) {
@@ -28,5 +29,6 @@ beta_regression = function(variable) {
 }
 
 variable = parameters[as.numeric(args[1])]
+print(variable)
 beta_regression(variable)
 
