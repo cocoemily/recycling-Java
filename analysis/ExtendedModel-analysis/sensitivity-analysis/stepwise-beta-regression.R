7b3783fcd0f7c7f1da7d2278b200e9a54b510e45
cocoemily <ec3307@nyu.edu>
2021-11-18 09:35:00 +0600

    stepwise regression

---
+++ analysis/ExtendedModel-analysis/sensitivity-analysis/stepwise-beta-regression.R
@@ -0,0 +1,23 @@
+library(betareg)
+library(tidyverse)
+library(lmtest)
+library(MASS)
+
+alldata = read_csv("/scratch/ec3307/recycling-Java/output/joined_sensitivity-data.csv")
+
+alldata = alldata %>% filter(size != "size") %>%
+  mutate(total.RI = as.numeric(total.RI), 
+         total.CR = as.numeric(total.CR)) %>%
+  group_by(max_use_intensity, max_artifact_carry, max_flake_size, max_nodules_size, blank_prob, scavenge_prob, overlap, mu, size_preference, flake_preference, min_suitable_flake_size, min_suitable_nodule_size, strict_selection) %>%
+  mutate(exp = cur_group_id()) %>%
+  filter(!is.na(total.RI))
+
+u_alldata = alldata %>% mutate(s.total.RI = ifelse(total.RI == 0, (total.RI + 0.0001), total.RI)) %>% mutate(s.total.RI = ifelse(s.total.RI == 1, (s.total.RI - 0.0001), s.total.RI))
+
+#full model
+breg1 = betareg(s.total.RI ~ model_year + max_use_intensity  + max_artifact_carry + max_flake_size + max_nodules_size + blank_prob + scavenge_prob + overlap + mu + size_preference + flake_preference + min_suitable_flake_size + min_suitable_nodule_size + strict_selection , data=u_alldata)
+
+#backwards stepwise regression
+step.model = stepAIC(breg1, direction = "backward", trace = FALSE)
+summary(step.model)
+
