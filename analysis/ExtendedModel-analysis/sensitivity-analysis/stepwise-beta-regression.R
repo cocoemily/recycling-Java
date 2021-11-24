1810b903ec4183962180415f2f92bb7c16d8b0a5
cocoemily <ec3307@nyu.edu>
2021-11-24 10:48:11 +0600

    adding in arg argument for stepGAIC

--- analysis/ExtendedModel-analysis/sensitivity-analysis/stepwise-beta-regression.R
+++ analysis/ExtendedModel-analysis/sensitivity-analysis/stepwise-beta-regression.R
@@ -18,7 +18,11 @@ u_alldata = alldata %>% mutate(s.total.RI = ifelse(total.RI == 0, (total.RI + 0.
 #full model
 breg1 = betareg(s.total.RI ~ model_year + max_use_intensity  + max_artifact_carry + max_flake_size + max_nodules_size + blank_prob + scavenge_prob + overlap + mu + size_preference + flake_preference + min_suitable_flake_size + min_suitable_nodule_size + strict_selection , data=u_alldata)
 
-#backwards stepwise regression
-step.model = stepGAIC(breg1, direction = "backward", trace = FALSE)
+parameters = c("max_use_intensity", "max_artifact_carry", "max_flake_size","max_nodules_size", "blank_prob", "scavenge_prob", "overlap","mu", "size_preference", "flake_preference","min_suitable_flake_size", "min_suitable_nodule_size", "strict_selection")
+
+#quasi backwards stepwise regression
+step.model = stepGAIC(breg1, direction = "backward", trace = FALSE, arg = "mean")
 summary(step.model)
+
+
 
