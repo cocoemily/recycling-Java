8ff30c9c90303465c50aa4b71395d1ed193defdf
cocoemily <ec3307@nyu.edu>
2022-01-14 11:39:57 +0600

    using absolute path name for HPC analysis

--- analysis/ExtendedModel-analysis/experiments/exp_render.R
+++ analysis/ExtendedModel-analysis/experiments/exp_render.R
@@ -3,5 +3,5 @@ library(tidyverse)
 library(data.table)
 library(knitr)
 
-alldata = read_csv("../output/joined_model_data.csv")
+alldata = read_csv("/scratch/ec3307/recycling-Java/output/joined_model_data.csv")
 rmarkdown::render("experiments-with-recycling.Rmd", output_file = "missing-recycling.html")
