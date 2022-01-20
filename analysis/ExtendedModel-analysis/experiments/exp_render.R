a8e91da2714cc2cdb39c956f8c1f8e8633f35e3a
cocoemily <ec3307@nyu.edu>
2022-01-20 09:37:18 +0600

    reading in data with different method to save memory

--- analysis/ExtendedModel-analysis/experiments/exp_render.R
+++ analysis/ExtendedModel-analysis/experiments/exp_render.R
@@ -1,7 +1,8 @@
 library(rmarkdown)
 library(tidyverse)
 library(data.table)
+library(readr)
 library(knitr)
 
-alldata = read_csv("/scratch/ec3307/recycling-Java/output/joined_model_data.csv")
+alldata = readr::read_csv("/scratch/ec3307/recycling-Java/output/joined_model_data.csv")
 rmarkdown::render("experiments-with-recycling.Rmd", output_file = "missing-recycling.html")
