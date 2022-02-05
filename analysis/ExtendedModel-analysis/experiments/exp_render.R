317b93b26aef5b76954a0fd7f00602ce1885aad0
cocoemily <ec3307@nyu.edu>
2022-02-05 07:06:54 -0500

    removing unnecessary crunch package

--- analysis/ExtendedModel-analysis/experiments/exp_render.R
+++ analysis/ExtendedModel-analysis/experiments/exp_render.R
@@ -5,4 +5,4 @@ library(readr)
 library(knitr)
 
 alldata = readr::read_csv("/scratch/ec3307/recycling-Java/output/joined_model_data.csv")
-rmarkdown::render("experiments-with-recycling.Rmd", output_file = "missing-recycling.html")
+rmarkdown::render("experiments-with-recycling.Rmd", output_file = "missing-recycling2.html")
