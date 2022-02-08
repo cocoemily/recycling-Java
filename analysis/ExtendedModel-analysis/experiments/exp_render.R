71a94a6b4957850ad1d70a4b5d611a7b04a373ce
cocoemily <ec3307@nyu.edu>
2022-02-08 14:56:09 +0600

    adding in preliminary analysis of where and why recycling is occuring in model space

--- analysis/ExtendedModel-analysis/experiments/exp_render.R
+++ analysis/ExtendedModel-analysis/experiments/exp_render.R
@@ -6,3 +6,5 @@ library(knitr)
 
 alldata = readr::read_csv("/scratch/ec3307/recycling-Java/output/joined_model_data.csv")
 rmarkdown::render("preliminary-data-analysis.Rmd", output_file = "prelim-data-analysis.html")
+
+#rmarkdown::render("where-why-recycling.Rmd", output_file = "recycling-correlations-at-mid-end.html")
