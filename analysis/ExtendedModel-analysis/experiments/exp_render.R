a4ad912681a84d3979278c0dea774394bf0a327d
cocoemily <ec3307@nyu.edu>
2022-02-16 04:55:19 -0500

    preliminary data analysis full results

--- analysis/ExtendedModel-analysis/experiments/exp_render.R
+++ analysis/ExtendedModel-analysis/experiments/exp_render.R
@@ -5,6 +5,6 @@ library(readr)
 library(knitr)
 
 alldata = readr::read_csv("/scratch/ec3307/recycling-Java/output/joined_model_data.csv")
-#rmarkdown::render("preliminary-data-analysis.Rmd", output_file = "prelim-data-analysis.html")
+rmarkdown::render("preliminary-data-analysis.Rmd", output_file = "prelim-data-analysis.html")
 
-rmarkdown::render("where-why-recycling.Rmd", output_file = "recycling-correlations-at-mid-end.html")
+#rmarkdown::render("where-why-recycling.Rmd", output_file = "recycling-correlations-at-mid-end.html")
