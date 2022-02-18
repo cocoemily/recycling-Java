43b458120aba1ca8164029b9a6df70c906bd2c20
cocoemily <ec3307@nyu.edu>
2022-02-18 09:24:21 +0600

    adding in parameter and output lists

--- analysis/ExtendedModel-analysis/experiments/missing-values-csv.R
+++ analysis/ExtendedModel-analysis/experiments/missing-values-csv.R
@@ -3,6 +3,10 @@ library(readr)
 
 alldata = readr::read_csv("/scratch/ec3307/recycling-Java/output/joined_model_data.csv")
 
+parameters = c("max_use_intensity", "max_artifact_carry", "max_flake_size","max_nodules_size", "blank_prob", "scavenge_prob", "overlap","mu", "size_preference", "flake_preference","min_suitable_flake_size", "min_suitable_nodule_size", "strict_selection")
+
+outputs = c("num.scav.events","total.recycled", "num.deposits",	"total.encounters",	"total.discards",	"total.manu.events", "total.retouches", "total.CR",	"total.RI")
+
 facet_data = alldata[,c(parameters, outputs)]
 
 missing = facet_data %>% group_by_at(parameters) %>% miss_var_summary %>%
