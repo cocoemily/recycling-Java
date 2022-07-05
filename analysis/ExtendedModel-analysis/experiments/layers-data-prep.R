8a9cbe4172e9e94486bfbfba1a5a39589c3197c6
cocoemily <ec3307@nyu.edu>
2022-07-05 10:52:59 +0600

    adding experiment number to layer data

--- analysis/ExtendedModel-analysis/experiments/layers-data-prep.R
+++ analysis/ExtendedModel-analysis/experiments/layers-data-prep.R
@@ -1,67 +1,16 @@
 library(tidyverse)
 
-parameters = c("max_use_intensity", "max_artifact_carry", "max_flake_size",
-               "max_nodules_size", "blank_prob", "scavenge_prob", "overlap",
-               "mu", "size_preference", "flake_preference",
-               "min_suitable_flake_size", "strict_selection")
+alldata = readr::read_csv("/scratch/ec3307/recycling-Java/output/joined_layers_data.csv")
+alldata = alldata[alldata$size != "size",]
+alldata = alldata[!is.na(alldata$max_artifact_carry),]
 
-df = data.frame(exp = numeric(),
-                max_use_intensity = numeric(),
-                max_artifact_carry = numeric(),
-                max_flake_size = numeric(),
-                max_nodules_size = numeric(),
-                blank_prob = numeric(),
-                scavenge_prob = numeric(),
-                overlap = numeric(),
-                mu = numeric(),
-                size_preference = character(),
-                flake_preference = character(),
-                min_suitable_flake_size = numeric(),
-                strict_selection = character(),
-                ri.cr.cor = numeric(), 
-                ri.nc.cor = numeric(),
-                ri.fc.cor = numeric(),
-                ri.nd.cor = numeric(),
-                ri.ns.cor = numeric(),
-                ri.ne.cor = numeric(),
-                ri.nm.cor = numeric(),
-                ri.nr.cor = numeric(),
-                ri.no.cor = numeric(),
-                ri.av.cor = numeric()
-)
+parameters = c("max_use_intensity", "max_artifact_carry", "max_flake_size","max_nodules_size", "blank_prob", "scavenge_prob", "overlap","mu", "size_preference", "flake_preference","min_suitable_flake_size", "min_suitable_nodule_size", "strict_selection")
 
-setwd("..")
-setwd("..")
-setwd("..")
-#for testing
-for(i in 1:3) {
-  
-  data = as.data.frame(fread(paste0("/Users/emilycoco/eclipse-workspace/recycling-Java/output/exp", i, "/run0_layers-data.csv")))
+exp = readr::read_csv("/scratch/ec3307/recycling-Java/run-scripts/ExtendedModel-model-runs/parameters.csv")
 
+exp = as.data.frame(exp[,c(1,6:18)])
+colnames(exp) = c("exp", parameters)
 
-#for(i in 1:8640) {
-  
-  #data = as.data.frame(fread(paste0("output/exp", i, "/layers-data.csv")))
-  
-  
-  
-  ri.cr.cor = cor(data$recycling.intensity, data$cortex.ratio, method = "spearman", use ="na.or.complete")
-  ri.nc.cor = cor(data$recycling.intensity, data$nodule.count, method = "spearman", use ="na.or.complete")
-  ri.fc.cor = cor(data$recycling.intensity, data$flake.count, method = "spearman", use ="na.or.complete")
-  ri.nd.cor = cor(data$recycling.intensity, data$num.discards, method = "spearman", use ="na.or.complete")
-  ri.ns.cor = cor(data$recycling.intensity, data$num.scavenge, method = "spearman", use ="na.or.complete")
-  ri.ne.cor = cor(data$recycling.intensity, data$num.encounters, method = "spearman", use ="na.or.complete")
-  ri.nm.cor = cor(data$recycling.intensity, data$num.manufacture, method = "spearman", use ="na.or.complete")
-  ri.nr.cor = cor(data$recycling.intensity, data$num.retouch, method = "spearman", use ="na.or.complete")
-  ri.no.cor = cor(data$recycling.intensity, data$num.retouch, method = "spearman", use ="na.or.complete")
-  ri.av.cor = cor(data$recycling.intensity, data$assemblage.vol, method = "spearman", use ="na.or.complete")
-  
-  newrow = c(i, unlist(data[1, parameters]), ri.cr.cor, ri.nc.cor, ri.fc.cor, 
-             ri.nd.cor, ri.ns.cor, ri.ne.cor, ri.nm.cor, ri.nr.cor, ri.no.cor, 
-             ri.av.cor )
-  
-  df[nrow(df) + 1, ] = newrow
-}
+layers = alldata %>% left_join(exp, by = parameters)
 
-setwd("analysis")
-
+readr::write_csv(layers[c(1:1000),], "test-layer-join.csv")
