d012e988b18bb951dcb69bcfd06037aab05aa47f
cocoemily <ec3307@nyu.edu>
2022-10-11 14:57:06 -0400

    reading all experiments from parameters file

--- analysis/ExtendedModel-analysis/experiments/variation-analysis.R
+++ analysis/ExtendedModel-analysis/experiments/variation-analysis.R
@@ -10,31 +10,11 @@ alldata = alldata[!is.na(alldata$max_artifact_carry),]
 
 parameters = c("max_use_intensity", "max_artifact_carry", "max_flake_size","max_nodules_size", "blank_prob", "scavenge_prob", "overlap","mu", "size_preference", "flake_preference","min_suitable_flake_size", "min_suitable_nodule_size", "strict_selection")
 
-# exp = alldata %>% group_by_at(parameters) %>%
-#   filter(row_number() == 1) %>%
-#   select_at(parameters) %>%
-#   filter(!is.na(max_use_intensity))
-
 allruns = alldata[which(alldata$model_year == 500000),]
-exp = distinct(allruns[parameters])
-print(nrow(exp2))
-
-#this gives all of the unique combinations of all of the parameters
-#but not all combinations are tested in my model
-# exp_grid = expand.grid(max_use_intensity = unique(alldata[[parameters[1]]]), 
-#                        max_artifact_carry = unique(alldata[[parameters[2]]]), 
-#                        max_flake_size = unique(alldata[[parameters[3]]]), 
-#                        max_nodules_size = unique(alldata[[parameters[4]]]), 
-#                        blank_prob = unique(alldata[[parameters[5]]]), 
-#                        scavenge_prob = unique(alldata[[parameters[6]]]), 
-#                        overlap = unique(alldata[[parameters[7]]]), 
-#                        mu = unique(alldata[[parameters[8]]]), 
-#                        size_preference = unique(alldata[[parameters[9]]]), 
-#                        flake_preference = unique(alldata[[parameters[10]]]), 
-#                        min_suitable_flake_size = unique(alldata[[parameters[11]]]), 
-#                        min_suitable_nodule_size = unique(alldata[[parameters[12]]]), 
-#                        strict_selection = unique(alldata[[parameters[13]]]))
-# test = distinct(exp_grid)
+exp_test = distinct(allruns[parameters])
+print(nrow(exp))
+exp = readr::read_csv("/scratch/ec3307/recycling-Java/run-scripts/ExtendedModel-model-runs/parameters.csv")
+colnames(exp) = c("exp", "run", "size", "start_year", "timestep", parameters, "erosion_ratio", "geo_freq", "total_steps")
 
 outputs = c("num.scav.events","total.recycled", "num.deposits",	"total.encounters",	"total.discards",	"total.manu.events", "total.retouches", "total.CR",	"total.RI")
 
@@ -103,20 +83,20 @@ modelyears = unique(alldata$model_year)
 
 
 for(row in 1:nrow(exp)) {
-  oneexp = alldata[ which(
-    alldata[parameters[1]] == as.numeric(exp[row, 1]) &
-      alldata[parameters[2]] == as.numeric(exp[row, 2]) &
-      alldata[parameters[3]] == as.numeric(exp[row, 3]) &
-      alldata[parameters[4]]== as.numeric(exp[row, 4]) &
-      alldata[parameters[5]] == as.numeric(exp[row, 5]) &
-      alldata[parameters[6]] == as.numeric(exp[row, 6]) &
-      alldata[parameters[7]] == as.numeric(exp[row, 7]) &
-      alldata[parameters[8]] == as.numeric(exp[row, 8]) &
-      alldata[parameters[9]] == as.numeric(exp[row, 9]) &
-      alldata[parameters[10]] == as.numeric(exp[row, 10]) &
-      alldata[parameters[11]] == as.numeric(exp[row, 11]) &
-      alldata[parameters[12]] == as.numeric(exp[row, 12]) &
-      alldata[parameters[13]] == as.numeric(exp[row, 13])
+  oneexp = alldata[which(
+    alldata[parameters[1]] == c(exp[row, parameters[1]]) &
+      alldata[parameters[2]] == c(exp[row, parameters[2]]) &
+      alldata[parameters[3]] == c(exp[row, parameters[3]]) &
+      alldata[parameters[4]] == c(exp[row, parameters[4]]) &
+      alldata[parameters[5]] == c(exp[row, parameters[5]]) &
+      alldata[parameters[6]] == c(exp[row, parameters[6]]) &
+      alldata[parameters[7]] == c(exp[row, parameters[7]]) &
+      alldata[parameters[8]] == c(exp[row, parameters[8]]) &
+      alldata[parameters[9]] == c(exp[row, parameters[9]]) &
+      alldata[parameters[10]] == c(exp[row, parameters[10]]) &
+      alldata[parameters[11]] == c(exp[row, parameters[11]]) &
+      alldata[parameters[12]] == c(exp[row, parameters[12]]) &
+      alldata[parameters[13]] == c(exp[row, parameters[13]])
   ),]
   
   for(year in 1:length(modelyears)) {
