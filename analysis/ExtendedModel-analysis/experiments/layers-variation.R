fe0cc9e11db598fca06afc8469c9ce0d5e86ca30
cocoemily <ec3307@nyu.edu>
2022-10-25 10:01:43 -0400

    removing last file from list.dirs output for layers variation analysis

--- analysis/ExtendedModel-analysis/experiments/layers-variation.R
+++ analysis/ExtendedModel-analysis/experiments/layers-variation.R
@@ -14,6 +14,7 @@ param_list = param_list[, c("exp", parameters)]
 dirs = list.dirs("/scratch/ec3307/recycling-Java/output/")
 ##remove folders refering to artifact data
 dirs = dirs[-c(1:3)]
+dirs = dirs[-length(dirs)]
 
 if(Sys.getenv("SLURM_CPUS_PER_TASK") != "") {
   ncores <- as.integer(Sys.getenv("SLURM_CPUS_PER_TASK"))
