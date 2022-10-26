2f792e34857c7f602bc980594ef3c62ef3a4e845
cocoemily <ec3307@nyu.edu>
2022-10-26 14:12:14 -0400

    testing data requirements for gridded correlation analysis of layers

--- analysis/ExtendedModel-analysis/experiments/layers-gridded-correlations.R
+++ analysis/ExtendedModel-analysis/experiments/layers-gridded-correlations.R
@@ -29,7 +29,6 @@ Sys.setenv(OMP_NUM_THREADS = "1")
 foreach (d=1:length(dirs)) %dopar% { 
   data = read_csv(paste0(dirs[d], "/layers-data.csv"), num_threads=1, show_col_types = F)
   print(dirs[d])
-  print(as.data.frame(head(data[parameters[1:2]])))
   gc()
   
   dirsplit = str_split(dirs[d], "\\/")[[1]]
