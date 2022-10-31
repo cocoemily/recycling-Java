c19b84c40b5be4dc8e7dc389528260de83d206d6
cocoemily <ec3307@nyu.edu>
2022-10-31 14:37:17 -0400

    analysis of results output separating overlap situations

--- analysis/ExtendedModel-analysis/experiments/results-analysis/artifact-exposure-results.R
+++ analysis/ExtendedModel-analysis/experiments/results-analysis/artifact-exposure-results.R
@@ -2,6 +2,7 @@
 
 library(tidyverse)
 library(ggthemes)
+library(jtools)
 
 theme_set(theme_bw())
 
@@ -12,14 +13,21 @@ exposure.data = read_csv("~/eclipse-workspace/recycling-Java/results/all-exposur
 ##end.conf.val = whether (TRUE) or not (FALSE) the recycled artifacts at the middle of model run 
 ## have ~significantly~ greater initial discard dates than the non-recycled artifacts
 
+two.tech = exposure.data %>% filter(overlap == 1)
+many.tech = exposure.data %>% filter(overlap == 2)
 
-exposure.plot = exposure.data %>% 
-  gather(key = "time", value = "signif", mid.conf.val, end.conf.val)
 
-ggplot(exposure.plot) +
-  geom_bar(aes(x = time, fill = signif), position = "dodge2") +
-  scale_x_discrete(labels = c("end of model run", "middle of model run"))
+plot_exposure_counts = function (exposure.data) {
+  exposure.plot = exposure.data %>% 
+    gather(key = "time", value = "signif", mid.conf.val, end.conf.val)
+  
+  ggplot(exposure.plot) +
+    geom_bar(aes(x = time, fill = signif), position = "dodge2") +
+    scale_x_discrete(labels = c("end of model run", "middle of model run"))
+}
 
+plot_exposure_counts(two.tech)
+plot_exposure_counts(many.tech)
 
 ##remove strict selection runs 
 updated.data = exposure.data %>% filter(strict_selection == FALSE)
@@ -29,15 +37,42 @@ up.exposure.plot = updated.data %>%
 
 ggplot(up.exposure.plot) +
   geom_bar(aes(x = time, fill = signif), position = "dodge2") +
-  scale_x_discrete(labels = c("end of model run", "middle of model run"))
-
+  scale_x_discrete(labels = c("end of model run", "middle of model run")) +
+  facet_wrap(~overlap)
 #exposure time of recycled artifacts are always greater than non-recycled artifacts unless the strictest of 
 #selection parameters is imposed in the model
+
 
 #examine what happens under strict choices
 strict.exp = exposure.data %>% filter(strict_selection == TRUE)
 
+two.tech.strict = strict.exp %>% filter(overlap == 1)
+many.tech.strict = strict.exp %>% filter(overlap == 2)
+
 #results will be likelihood of TRUE values
+two.tech.fit = glm(mid.conf.val ~ as.factor(max_use_intensity) + as.factor(max_artifact_carry) + 
+                     as.factor(max_flake_size) + as.factor(max_nodules_size) + 
+                     as.factor(blank_prob) + as.factor(scavenge_prob) + 
+                     as.factor(mu) + as.factor(size_preference) + as.factor(flake_preference) + 
+                     as.factor(min_suitable_flake_size) + as.factor(min_suitable_nodule_size), 
+                   data = two.tech.strict, family = binomial)
+summary(two.tech.fit)
+
+
+many.tech.fit = glm(mid.conf.val ~ as.factor(max_use_intensity) + as.factor(max_artifact_carry) + 
+                     as.factor(max_flake_size) + as.factor(max_nodules_size) + 
+                     as.factor(blank_prob) + as.factor(scavenge_prob) + 
+                     as.factor(mu) + as.factor(size_preference) + as.factor(flake_preference) +
+                     as.factor(min_suitable_flake_size) + as.factor(min_suitable_nodule_size), 
+                   data = many.tech.strict, family = binomial)
+summary(many.tech.fit)
+
+
+plot_summs(two.tech.fit, many.tech.fit, model.names = c("Two technologies", "Many technologies"))
+export_summs(two.tech.fit, many.tech.fit, scale = T, error_format = "[{conf.low}, {conf.high}]", 
+             model.names = c("Two technologies", "Many technologies"))
+
+##NOT SPLIT BY OVERLAP
 bilog = glm(mid.conf.val ~ as.factor(max_use_intensity) + as.factor(max_artifact_carry) + 
               as.factor(max_flake_size) + as.factor(max_nodules_size) + 
               as.factor(blank_prob) + as.factor(scavenge_prob) + as.factor(overlap) + 
--- analysis/ExtendedModel-analysis/experiments/results-analysis/spat-layers-variation-results.R
+++ analysis/ExtendedModel-analysis/experiments/results-analysis/spat-layers-variation-results.R
@@ -4,9 +4,12 @@ library(tidyverse)
 library(rcompanion)
 library(fitdistrplus)
 library(betareg)
+library(jtools)
 
 #### ANALYSIS OF VARIATION OF OUTPUT VARIABLES BETWEEN GRID SQUARES ACROSS MODEL RUNS ####
 layer.var = read_csv("~/eclipse-workspace/recycling-Java/results/all-layer-variation-output.csv")
+two.tech.layers = layer.var %>% filter(overlap == 1)
+many.tech.layers = layer.var %>% filter(overlap == 2)
 
 summary(layer.var$nod.cnt.avg.sd)
 summary(layer.var$flk.cnt.avg.sd)
@@ -18,13 +21,11 @@ summary(layer.var$num.manu.avg.sd)
 summary(layer.var$num.occp.avg.sd)
 summary(layer.var$num.ret.avg.sd)
 summary(layer.var$num.scvg.avg.sd)
-
 ##number of discard and scavenging events are very highly variably between model runs of same experiment
 ##recycling intensity has low variation between model runs of same experiment, same with cortex ratios
 ##flake counts and nodule counts are fairly variable as well between model runs
 
 plotNormalHistogram(layer.var$nod.cnt.avg.sd)
-descdist(layer.var$nod.cnt.avg.sd) #lognormal
 plotNormalHistogram(log(layer.var$flk.cnt.avg.sd))
 plotNormalHistogram(layer.var$cr.avg.sd) ##zero inflated?
 plotNormalHistogram(layer.var$ri.avg.sd) ##zero inflated?
@@ -41,6 +42,44 @@ summary(flk.cnt.reg)
 
 nod.cnt.reg = lm(nod.cnt.avg.sd ~ . , data = layer.var[,c(2:14, 16)])
 summary(nod.cnt.reg)
+
+
+#### Two technology types ####
+plotNormalHistogram(two.tech.layers$nod.cnt.avg.sd) #normal
+plotNormalHistogram(log(two.tech.layers$flk.cnt.avg.sd)) #lognormal
+plotNormalHistogram(log(two.tech.layers$cr.avg.sd)) 
+plotNormalHistogram(two.tech.layers$ri.avg.sd) 
+plotNormalHistogram(two.tech.layers$num.dis.avg.sd) ##zero inflated?
+plotNormalHistogram(two.tech.layers$num.enc.avg.sd) #multimodal
+plotNormalHistogram(two.tech.layers$num.manu.avg.sd) #bimodal
+plotNormalHistogram(two.tech.layers$num.occp.avg.sd) #multimodal
+plotNormalHistogram(two.tech.layers$num.ret.avg.sd) #uniform-ish
+plotNormalHistogram(two.tech.layers$num.scvg.avg.sd) #zero inflated
+
+flk.cnt.reg = glm(log(flk.cnt.avg.sd) ~ . , data = two.tech.layers[,c(2:7, 9:14, 18)])
+summary(flk.cnt.reg)
+
+nd.cnt.rg = lm(nod.cnt.avg.sd ~ . , data = two.tech.layers[,c(2:7, 9:14, 16)])
+summary(nd.cnt.rg)
+
+cr.rg = lm(log(cr.avg.sd) ~ . , data = two.tech.layers[,c(2:7, 9:14, 20)])
+plot(cr.rg, which = 2)
+summary(cr.rg)
+
+ri.rg = lm(ri.avg.sd ~ . , data = two.tech.layers[,c(2:7, 9:14, 22)])
+
+
+#### Many technology types ####
+plotNormalHistogram(many.tech.layers$nod.cnt.avg.sd) #normal
+plotNormalHistogram(log(many.tech.layers$flk.cnt.avg.sd)) #lognormal
+plotNormalHistogram(many.tech.layers$cr.avg.sd) ##zero inflated?
+plotNormalHistogram(many.tech.layers$ri.avg.sd) ##zero inflated normal?
+plotNormalHistogram(many.tech.layers$num.dis.avg.sd) ##zero inflated?
+plotNormalHistogram(many.tech.layers$num.enc.avg.sd) #multimodal
+plotNormalHistogram(many.tech.layers$num.manu.avg.sd) #bimodal
+plotNormalHistogram(many.tech.layers$num.occp.avg.sd) #multimodal
+plotNormalHistogram(many.tech.layers$num.ret.avg.sd) #uniform-ish
+plotNormalHistogram(many.tech.layers$num.scvg.avg.sd) #zero inflated
 
 
 
