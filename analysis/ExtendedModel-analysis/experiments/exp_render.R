0613ef8631bd04f4caf197bc86bdeb8883350911
cocoemily <ec3307@nyu.edu>
2022-11-01 14:59:55 -0400

    artifact retouch intensity results+ changes to data request

--- analysis/ExtendedModel-analysis/experiments/exp_render.R
+++ analysis/ExtendedModel-analysis/experiments/exp_render.R
@@ -5,6 +5,6 @@ library(readr)
 library(knitr)
 
 alldata = readr::read_csv("/scratch/ec3307/recycling-Java/output/joined_model_data.csv")
-#rmarkdown::render("preliminary-data-analysis.Rmd", output_file = "prelim-data-analysis.html")
+rmarkdown::render("preliminary-data-analysis.Rmd", output_file = "prelim-data-analysis.html")
 
-rmarkdown::render("output-analysis.Rmd", output_file = "output-correlations.html")
+#rmarkdown::render("output-analysis.Rmd", output_file = "output-correlations.html")
