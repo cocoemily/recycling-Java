fc8cc4d3c2e83ece9b746c1cae6e22c86aefc4af
cocoemily <ec3307@nyu.edu>
2022-12-08 14:50:16 -0500

    adding change over time scripts for new model output variables by model year

---
+++ analysis/ExtendedModel-analysis/experiments/model-analysis/change-over-time-discards.R
@@ -0,0 +1,139 @@
+### Analysis of model outcomes over time by overlap, mobility, and selection
+
+library(tidyverse)
+library(ggthemes)
+
+theme_set(theme_bw())
+
+alldata = readr::read_csv("/scratch/ec3307/recycling-Java/output/joined_model_data.csv")
+alldata = alldata[alldata$size != "size",]
+alldata = alldata[!is.na(alldata$max_artifact_carry),]
+
+parameters = c("max_use_intensity", "max_artifact_carry", "max_flake_size","max_nodules_size", "blank_prob", "scavenge_prob", "size_preference", "flake_preference","min_suitable_flake_size", "min_suitable_nodule_size", "strict_selection")
+grouping_params = c("mu", "overlap")
+
+# exp = readr::read_csv("/scratch/ec3307/recycling-Java/run-scripts/ExtendedModel-model-runs/parameters.csv")
+# colnames(exp) = c("exp", "run", "size", "start_year", "timestep", parameters, "erosion_ratio", "geo_freq", "total_steps")
+
+outputs = c("num.scav.events","total.recycled", "num.deposits",	"total.encounters",	"total.discards",	"total.manu.events", "total.retouches", "total.CR",	"total.RI")
+
+move_outputs = c("num.deposits", "total.encounters")
+scavenge_outputs = c("num.scav.events", "total.recycled", "total.discards", "total.manu.events", "total.retouches")
+
+two.tech = alldata[which(alldata$overlap == 1),]
+multi.tech = alldata[which(alldata$overlap == 2),]
+mu.1 = alldata[which(alldata$mu == 1),]
+mu.2 = alldata[which(alldata$mu == 2),]
+mu.3 = alldata[which(alldata$mu == 3),]
+flake.selection = alldata[which(alldata$flake_preference == TRUE),]
+nodule.selection = alldata[which(alldata$flake_preference == FALSE),]
+
+rm(alldata)
+
+###OVERLAP####
+avg.two.tech = two.tech %>%
+  group_by(model_year) %>%
+  summarize(mean.disc = mean(num.discard.events),
+            sd.disc = sd(num.discard.events),
+            n.disc = n()) %>%
+  mutate(overlap = 1,
+         se.disc = sd.disc / sqrt(n.disc),
+         lower.ci.disc = mean.disc - qt(1 - (0.05 / 2), n.disc - 1) * se.disc,
+         upper.ci.disc = mean.disc + qt(1 - (0.05 / 2), n.disc - 1) * se.disc)
+avg.multi.tech = multi.tech %>%
+  group_by(model_year) %>%
+  summarize(mean.disc = mean(num.discard.events),
+            sd.disc = sd(num.discard.events),
+            n.disc = n()) %>%
+  mutate(overlap = 2,
+         se.disc = sd.disc / sqrt(n.disc),
+         lower.ci.disc = mean.disc - qt(1 - (0.05 / 2), n.disc - 1) * se.disc,
+         upper.ci.disc = mean.disc + qt(1 - (0.05 / 2), n.disc - 1) * se.disc)
+
+oplot = ggplot() +
+  geom_line(data = avg.two.tech, aes(x = model_year, y = mean.disc, color = as.factor(overlap))) +
+  geom_ribbon(data = avg.two.tech, aes(x = model_year, ymin = lower.ci.disc, ymax = upper.ci.disc), alpha = 0.2) +
+  geom_line(data = avg.multi.tech, aes(x = model_year, y = mean.disc, color = as.factor(overlap))) +
+  geom_ribbon(data = avg.multi.tech, aes(x = model_year, ymin = lower.ci.disc, ymax = upper.ci.disc), alpha = 0.2) +
+  scale_x_reverse() +
+  scale_color_colorblind() +
+  labs(color = "overlap parameter", x = "model year", y = "average number of discard events")
+
+ggsave(filename = "discard-trend-by-overlap.png", oplot, dpi = 300)
+
+####MU####
+avg.mu.1 = mu.1 %>%
+  group_by(model_year) %>%
+  summarize(mean.disc = mean(num.discard.events),
+            sd.disc = sd(num.discard.events),
+            n.disc = n()) %>%
+  mutate(mu = 1,
+         se.disc = sd.disc / sqrt(n.disc),
+         lower.ci.disc = mean.disc - qt(1 - (0.05 / 2), n.disc - 1) * se.disc,
+         upper.ci.disc = mean.disc + qt(1 - (0.05 / 2), n.disc - 1) * se.disc)
+avg.mu.2 = mu.2 %>%
+  group_by(model_year) %>%
+  summarize(mean.disc = mean(num.discard.events),
+            sd.disc = sd(num.discard.events),
+            n.disc = n()) %>%
+  mutate(mu = 2,
+         se.disc = sd.disc / sqrt(n.disc),
+         lower.ci.disc = mean.disc - qt(1 - (0.05 / 2), n.disc - 1) * se.disc,
+         upper.ci.disc = mean.disc + qt(1 - (0.05 / 2), n.disc - 1) * se.disc)
+avg.mu.3 = mu.3 %>%
+  group_by(model_year) %>%
+  summarize(mean.disc = mean(num.discard.events),
+            sd.disc = sd(num.discard.events),
+            n.disc = n()) %>%
+  mutate(mu = 3,
+         se.disc = sd.disc / sqrt(n.disc),
+         lower.ci.disc = mean.disc - qt(1 - (0.05 / 2), n.disc - 1) * se.disc,
+         upper.ci.disc = mean.disc + qt(1 - (0.05 / 2), n.disc - 1) * se.disc)
+
+mplot = ggplot() +
+  geom_line(data = avg.mu.1, aes(x = model_year, y = mean.disc, color = as.factor(mu))) +
+  geom_ribbon(data = avg.mu.1, aes(x = model_year, ymin = lower.ci.disc, ymax = upper.ci.disc), alpha = 0.2) +
+  geom_line(data = avg.mu.2, aes(x = model_year, y = mean.disc, color = as.factor(mu))) +
+  geom_ribbon(data = avg.mu.2, aes(x = model_year, ymin = lower.ci.disc, ymax = upper.ci.disc), alpha = 0.2) +
+  geom_line(data = avg.mu.3, aes(x = model_year, y = mean.disc, color = as.factor(mu))) +
+  geom_ribbon(data = avg.mu.3, aes(x = model_year, ymin = lower.ci.disc, ymax = upper.ci.disc), alpha = 0.2) +
+  scale_x_reverse() +
+  scale_color_colorblind() +
+  labs(color = "mu parameter", x = "model year", y = "average number of discard events")
+
+ggsave(filename = "discard-trend-by-mu.png", mplot, dpi = 300)
+
+
+####SELECTION####
+avg.flk.select = flake.selection %>%
+  group_by(size_preference, strict_selection, model_year) %>%
+  summarize(mean.disc = mean(num.discard.events), 
+            sd.disc = sd(num.discard.events), 
+            n.disc = n()) %>%
+  mutate(flake_preference = TRUE, 
+         se.disc = sd.disc / sqrt(n.disc),
+         lower.ci.disc = mean.disc - qt(1 - (0.05 / 2), n.disc - 1) * se.disc, 
+         upper.ci.disc = mean.disc + qt(1 - (0.05 / 2), n.disc - 1) * se.disc)
+  
+
+avg.nod.select = nodule.selection %>%
+  group_by(size_preference, strict_selection, model_year) %>%
+  summarize(mean.disc = mean(num.discard.events), 
+            sd.disc = sd(num.discard.events), 
+            n.disc = n()) %>%
+  mutate(flake_preference = FALSE, 
+         se.disc = sd.disc / sqrt(n.disc),
+         lower.ci.disc = mean.disc - qt(1 - (0.05 / 2), n.disc - 1) * se.disc, 
+         upper.ci.disc = mean.disc + qt(1 - (0.05 / 2), n.disc - 1) * se.disc)
+
+splot = ggplot() +
+  geom_line(data = avg.flk.select, aes(x = model_year, y = mean.disc, color = as.factor(flake_preference))) + 
+  geom_ribbon(data = avg.flk.select, aes(x = model_year, ymin = lower.ci.disc, ymax = upper.ci.disc), alpha = 0.2) +
+  geom_line(data = avg.nod.select, aes(x = model_year, y = mean.disc, color = as.factor(flake_preference))) +
+  geom_ribbon(data = avg.nod.select, aes(x = model_year, ymin = lower.ci.disc, ymax = upper.ci.disc), alpha = 0.2) +
+  facet_grid(size_preference ~ strict_selection, labeller = label_both) +
+  scale_x_reverse() +
+  scale_color_colorblind() +
+  labs(color = "flake preference", x = "model year", y = "average number of discard events")
+
+ggsave(filename = "discard-trend-by-selection.png", splot, dpi = 300)
---
+++ analysis/ExtendedModel-analysis/experiments/model-analysis/change-over-time-recycling-events.R
@@ -0,0 +1,139 @@
+### Analysis of model outcomes over time by overlap, mobility, and selection
+
+library(tidyverse)
+library(ggthemes)
+
+theme_set(theme_bw())
+
+alldata = readr::read_csv("/scratch/ec3307/recycling-Java/output/joined_model_data.csv")
+alldata = alldata[alldata$size != "size",]
+alldata = alldata[!is.na(alldata$max_artifact_carry),]
+
+parameters = c("max_use_intensity", "max_artifact_carry", "max_flake_size","max_nodules_size", "blank_prob", "scavenge_prob", "size_preference", "flake_preference","min_suitable_flake_size", "min_suitable_nodule_size", "strict_selection")
+grouping_params = c("mu", "overlap")
+
+# exp = readr::read_csv("/scratch/ec3307/recycling-Java/run-scripts/ExtendedModel-model-runs/parameters.csv")
+# colnames(exp) = c("exp", "run", "size", "start_year", "timestep", parameters, "erosion_ratio", "geo_freq", "total_steps")
+
+outputs = c("num.scav.events","total.recycled", "num.deposits",	"total.encounters",	"total.discards",	"total.manu.events", "total.retouches", "total.CR",	"total.RI")
+
+move_outputs = c("num.deposits", "total.encounters")
+scavenge_outputs = c("num.scav.events", "total.recycled", "total.discards", "total.manu.events", "total.retouches")
+
+two.tech = alldata[which(alldata$overlap == 1),]
+multi.tech = alldata[which(alldata$overlap == 2),]
+mu.1 = alldata[which(alldata$mu == 1),]
+mu.2 = alldata[which(alldata$mu == 2),]
+mu.3 = alldata[which(alldata$mu == 3),]
+flake.selection = alldata[which(alldata$flake_preference == TRUE),]
+nodule.selection = alldata[which(alldata$flake_preference == FALSE),]
+
+rm(alldata)
+
+###OVERLAP####
+avg.two.tech = two.tech %>%
+  group_by(model_year) %>%
+  summarize(mean.ro = mean(num.rcycl.obj.made),
+            sd.ro = sd(num.rcycl.obj.made),
+            n.ro = n()) %>%
+  mutate(overlap = 1,
+         se.ro = sd.ro / sqrt(n.ro),
+         lower.ci.ro = mean.ro - qt(1 - (0.05 / 2), n.ro - 1) * se.ro,
+         upper.ci.ro = mean.ro + qt(1 - (0.05 / 2), n.ro - 1) * se.ro)
+avg.multi.tech = multi.tech %>%
+  group_by(model_year) %>%
+  summarize(mean.ro = mean(num.rcycl.obj.made),
+            sd.ro = sd(num.rcycl.obj.made),
+            n.ro = n()) %>%
+  mutate(overlap = 2,
+         se.ro = sd.ro / sqrt(n.ro),
+         lower.ci.ro = mean.ro - qt(1 - (0.05 / 2), n.ro - 1) * se.ro,
+         upper.ci.ro = mean.ro + qt(1 - (0.05 / 2), n.ro - 1) * se.ro)
+
+oplot = ggplot() +
+  geom_line(data = avg.two.tech, aes(x = model_year, y = mean.ro, color = as.factor(overlap))) +
+  geom_ribbon(data = avg.two.tech, aes(x = model_year, ymin = lower.ci.ro, ymax = upper.ci.ro), alpha = 0.2) +
+  geom_line(data = avg.multi.tech, aes(x = model_year, y = mean.ro, color = as.factor(overlap))) +
+  geom_ribbon(data = avg.multi.tech, aes(x = model_year, ymin = lower.ci.ro, ymax = upper.ci.ro), alpha = 0.2) +
+  scale_x_reverse() +
+  scale_color_colorblind() +
+  labs(color = "overlap parameter", x = "model year", y = "average number of recycled objects created")
+
+ggsave(filename = "recycled-objects-trend-by-overlap.png", oplot, dpi = 300)
+
+####MU####
+avg.mu.1 = mu.1 %>%
+  group_by(model_year) %>%
+  summarize(mean.ro = mean(num.rcycl.obj.made),
+            sd.ro = sd(num.rcycl.obj.made),
+            n.ro = n()) %>%
+  mutate(mu = 1,
+         se.ro = sd.ro / sqrt(n.ro),
+         lower.ci.ro = mean.ro - qt(1 - (0.05 / 2), n.ro - 1) * se.ro,
+         upper.ci.ro = mean.ro + qt(1 - (0.05 / 2), n.ro - 1) * se.ro)
+avg.mu.2 = mu.2 %>%
+  group_by(model_year) %>%
+  summarize(mean.ro = mean(num.rcycl.obj.made),
+            sd.ro = sd(num.rcycl.obj.made),
+            n.ro = n()) %>%
+  mutate(mu = 2,
+         se.ro = sd.ro / sqrt(n.ro),
+         lower.ci.ro = mean.ro - qt(1 - (0.05 / 2), n.ro - 1) * se.ro,
+         upper.ci.ro = mean.ro + qt(1 - (0.05 / 2), n.ro - 1) * se.ro)
+avg.mu.3 = mu.3 %>%
+  group_by(model_year) %>%
+  summarize(mean.ro = mean(num.rcycl.obj.made),
+            sd.ro = sd(num.rcycl.obj.made),
+            n.ro = n()) %>%
+  mutate(mu = 3,
+         se.ro = sd.ro / sqrt(n.ro),
+         lower.ci.ro = mean.ro - qt(1 - (0.05 / 2), n.ro - 1) * se.ro,
+         upper.ci.ro = mean.ro + qt(1 - (0.05 / 2), n.ro - 1) * se.ro)
+
+mplot = ggplot() +
+  geom_line(data = avg.mu.1, aes(x = model_year, y = mean.ro, color = as.factor(mu))) +
+  geom_ribbon(data = avg.mu.1, aes(x = model_year, ymin = lower.ci.ro, ymax = upper.ci.ro), alpha = 0.2) +
+  geom_line(data = avg.mu.2, aes(x = model_year, y = mean.ro, color = as.factor(mu))) +
+  geom_ribbon(data = avg.mu.2, aes(x = model_year, ymin = lower.ci.ro, ymax = upper.ci.ro), alpha = 0.2) +
+  geom_line(data = avg.mu.3, aes(x = model_year, y = mean.ro, color = as.factor(mu))) +
+  geom_ribbon(data = avg.mu.3, aes(x = model_year, ymin = lower.ci.ro, ymax = upper.ci.ro), alpha = 0.2) +
+  scale_x_reverse() +
+  scale_color_colorblind() +
+  labs(color = "mu parameter", x = "model year", y = "average number of recycled objects created")
+
+ggsave(filename = "recycled-objects-trend-by-mu.png", mplot, dpi = 300)
+
+
+####SELECTION####
+avg.flk.select = flake.selection %>%
+  group_by(size_preference, strict_selection, model_year) %>%
+  summarize(mean.ro = mean(num.rcycl.obj.made), 
+            sd.ro = sd(num.rcycl.obj.made), 
+            n.ro = n()) %>%
+  mutate(flake_preference = TRUE, 
+         se.ro = sd.ro / sqrt(n.ro),
+         lower.ci.ro = mean.ro - qt(1 - (0.05 / 2), n.ro - 1) * se.ro, 
+         upper.ci.ro = mean.ro + qt(1 - (0.05 / 2), n.ro - 1) * se.ro)
+  
+
+avg.nod.select = nodule.selection %>%
+  group_by(size_preference, strict_selection, model_year) %>%
+  summarize(mean.ro = mean(num.rcycl.obj.made), 
+            sd.ro = sd(num.rcycl.obj.made), 
+            n.ro = n()) %>%
+  mutate(flake_preference = FALSE, 
+         se.ro = sd.ro / sqrt(n.ro),
+         lower.ci.ro = mean.ro - qt(1 - (0.05 / 2), n.ro - 1) * se.ro, 
+         upper.ci.ro = mean.ro + qt(1 - (0.05 / 2), n.ro - 1) * se.ro)
+
+splot = ggplot() +
+  geom_line(data = avg.flk.select, aes(x = model_year, y = mean.ro, color = as.factor(flake_preference))) + 
+  geom_ribbon(data = avg.flk.select, aes(x = model_year, ymin = lower.ci.ro, ymax = upper.ci.ro), alpha = 0.2) +
+  geom_line(data = avg.nod.select, aes(x = model_year, y = mean.ro, color = as.factor(flake_preference))) +
+  geom_ribbon(data = avg.nod.select, aes(x = model_year, ymin = lower.ci.ro, ymax = upper.ci.ro), alpha = 0.2) +
+  facet_grid(size_preference ~ strict_selection, labeller = label_both) +
+  scale_x_reverse() +
+  scale_color_colorblind() +
+  labs(color = "flake preference", x = "model year", y = "average number of recycled objects created")
+
+ggsave(filename = "recycled-objects-trend-by-selection.png", splot, dpi = 300)
