442454d00f899e121f557e1e80b79dde91de8b5d
cocoemily <ec3307@nyu.edu>
2022-12-09 13:32:42 -0500

    updates to sbatch and render scripts from HPC

--- analysis/ExtendedModel-analysis/experiments/model-analysis/exp_render.R
+++ analysis/ExtendedModel-analysis/experiments/model-analysis/exp_render.R
@@ -5,7 +5,7 @@ library(readr)
 library(knitr)
 
 alldata = readr::read_csv("/scratch/ec3307/recycling-Java/output/joined_model_data.csv")
-#rmarkdown::render("preliminary-data-analysis-two-techs.Rmd", output_file = "prelim-analysis-two-techs.html")
-rmarkdown::render("preliminary-data-analysis-many-techs.Rmd", output_file = "prelim-analysis-many-techs.html")
+rmarkdown::render("preliminary-data-analysis-two-techs.Rmd", output_file = "prelim-analysis-two-techs.html")
+#rmarkdown::render("preliminary-data-analysis-many-techs.Rmd", output_file = "prelim-analysis-many-techs.html")
 
 #rmarkdown::render("output-analysis.Rmd", output_file = "output-correlations.html")
