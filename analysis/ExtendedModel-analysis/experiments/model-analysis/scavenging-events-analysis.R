1b955ee5ad7dd36fc643f04b87a1789506ad26ca
cocoemily <ec3307@nyu.edu>
2023-01-11 09:53:53 -0500

    looking at num scavenging events distribution

--- analysis/ExtendedModel-analysis/experiments/model-analysis/scavenging-events-analysis.R
+++ analysis/ExtendedModel-analysis/experiments/model-analysis/scavenging-events-analysis.R
@@ -16,7 +16,6 @@ alldata = alldata[alldata$size != "size",]
 alldata = alldata[!is.na(alldata$max_artifact_carry),]
 
 parameters = c("max_use_intensity", "max_artifact_carry", "max_flake_size","max_nodules_size", "blank_prob", "scavenge_prob", "size_preference", "flake_preference","min_suitable_flake_size", "min_suitable_nodule_size", "strict_selection")
-grouping_params = c("mu", "overlap")
 
 outputs = colnames(alldata[c(22:34)])
 step.outputs = outputs[1:5]
@@ -25,26 +24,32 @@ scvg.data = alldata[c(parameters, grouping_params, "model_year", "num.scav.event
 rm(alldata)
 
 
-flake.selection = scvg.data[which(scvg.data$flake_preference == TRUE),]
-nodule.selection = scvg.data[which(scvg.data$flake_preference == FALSE),]
+summary(scvg.data$num.scav.events)
 
-##scavenging events are zero-inflated negative binomial distribution
+print(scvg.data[which(scvg.data$num.scav.events > 1000)],)
 
-mu.1 = scvg.data[which(scvg.data$mu == 1),]
-mu.2 = scvg.data[which(scvg.data$mu == 2),]
-mu.3 = scvg.data[which(scvg.data$mu == 3),]
-##hold overlap constant
-mu.1 = mu.1[which(mu.1$overlap == 1),]
-mu.2 = mu.2[which(mu.2$overlap == 1),]
-mu.3 = mu.3[which(mu.3$overlap == 1),]
 
-fit1 = zeroinfl(num.scav.events ~ ., data = mu.1[c(1:6,9:15)])
-fit2 = zeroinfl(num.scav.events ~ ., data = mu.2[c(1:6,9:15)])
-fit3 = zeroinfl(num.scav.events ~ ., data = mu.3[c(1:6,9:15)])
+# flake.selection = scvg.data[which(scvg.data$flake_preference == TRUE),]
+# nodule.selection = scvg.data[which(scvg.data$flake_preference == FALSE),]
+# 
+# ##scavenging events are zero-inflated negative binomial distribution
+# 
+# mu.1 = scvg.data[which(scvg.data$mu == 1),]
+# mu.2 = scvg.data[which(scvg.data$mu == 2),]
+# mu.3 = scvg.data[which(scvg.data$mu == 3),]
+# ##hold overlap constant
+# mu.1 = mu.1[which(mu.1$overlap == 1),]
+# mu.2 = mu.2[which(mu.2$overlap == 1),]
+# mu.3 = mu.3[which(mu.3$overlap == 1),]
 
-ggsave(filename = "scavenging-event-regressions-by-mu.png",
-  plot_summs(fit1, fit2, fit3, model.names = c("mu = 1", "mu = 2", "mu = 3")), 
-  dpi = 300
-)
+
+# fit1 = zeroinfl(num.scav.events ~ ., data = mu.1[c(1:6,9:15)])
+# fit2 = zeroinfl(num.scav.events ~ ., data = mu.2[c(1:6,9:15)])
+# fit3 = zeroinfl(num.scav.events ~ ., data = mu.3[c(1:6,9:15)])
+# 
+# ggsave(filename = "scavenging-event-regressions-by-mu.png",
+#   plot_summs(fit1, fit2, fit3, model.names = c("mu = 1", "mu = 2", "mu = 3")), 
+#   dpi = 300
+# )
 
 
