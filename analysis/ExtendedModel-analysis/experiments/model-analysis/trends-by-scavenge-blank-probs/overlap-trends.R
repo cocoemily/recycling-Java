94545a14990228976010b1382516a0b11e4bdca5
cocoemily <ec3307@nyu.edu>
2023-03-01 16:38:20 -0500

    get overlap trend graphs

---
+++ analysis/ExtendedModel-analysis/experiments/model-analysis/trends-by-scavenge-blank-probs/overlap-trends.R
@@ -0,0 +1,95 @@
+### Analysis of model outcomes over time by scavenging and blank probabilities
+
+library(tidyverse)
+library(ggthemes)
+library(ggpubr)
+library(scales)
+
+theme_set(theme_bw())
+
+alldata = readr::read_csv("/scratch/ec3307/recycling-Java/output/joined_model_data.csv")
+alldata = alldata[alldata$size != "size",]
+alldata = alldata[!is.na(alldata$max_artifact_carry),]
+
+two.tech = alldata[which(alldata$overlap == 1),]
+multi.tech = alldata[which(alldata$overlap == 2),]
+
+rm(alldata)
+
+####overlap####
+#####recycling intensity####
+avg.two.tech = two.tech %>%
+  group_by(model_year, blank_prob, scavenge_prob) %>%
+  summarize(mean.RI = mean(total.RI),
+            sd.RI = sd(total.RI),
+            n.RI = n()) %>%
+  mutate(overlap = 1,
+         se.RI = sd.RI / sqrt(n.RI),
+         lower.ci.RI = mean.RI - qt(1 - (0.05 / 2), n.RI - 1) * se.RI,
+         upper.ci.RI = mean.RI + qt(1 - (0.05 / 2), n.RI - 1) * se.RI)
+avg.multi.tech = multi.tech %>%
+  group_by(model_year, blank_prob, scavenge_prob) %>%
+  summarize(mean.RI = mean(total.RI),
+            sd.RI = sd(total.RI),
+            n.RI = n()) %>%
+  mutate(overlap = 2,
+         se.RI = sd.RI / sqrt(n.RI),
+         lower.ci.RI = mean.RI - qt(1 - (0.05 / 2), n.RI - 1) * se.RI,
+         upper.ci.RI = mean.RI + qt(1 - (0.05 / 2), n.RI - 1) * se.RI)
+
+p1 = ggplot() +
+  geom_line(data = avg.two.tech, aes(x = model_year, y = mean.RI, color = as.factor(overlap))) +
+  geom_ribbon(data = avg.two.tech, aes(x = model_year, ymin = lower.ci.RI, ymax = upper.ci.RI), alpha = 0.2) +
+  geom_line(data = avg.multi.tech, aes(x = model_year, y = mean.RI, color = as.factor(overlap))) +
+  geom_ribbon(data = avg.multi.tech, aes(x = model_year, ymin = lower.ci.RI, ymax = upper.ci.RI), alpha = 0.2) +
+  facet_grid(blank_prob ~ scavenge_prob, labeller = labeller(
+    blank_prob = blank.labs, scavenge_prob = scvg.labs
+  )) +
+  scale_x_reverse(breaks = c(500000, 350000, 200000), 
+                  labels = label_number(scale_cut = cut_short_scale())) +
+  scale_y_continuous(labels = label_number(accuracy = 0.01)) +
+  scale_color_colorblind() +
+  labs(color = "overlap parameter", x = "model year", y = "average recycling intensity") +
+  theme(strip.text = element_text(size = 5))
+
+save(p1, file = "ri-overlap.rdata")
+
+rm(avg.two.tech, avg.multi.tech)
+
+#####recycling events####
+avg.two.tech = two.tech %>%
+  group_by(model_year, blank_prob, scavenge_prob) %>%
+  summarize(mean.ro = mean(num.rcycl.obj.made),
+            sd.ro = sd(num.rcycl.obj.made),
+            n.ro = n()) %>%
+  mutate(overlap = 1,
+         se.ro = sd.ro / sqrt(n.ro),
+         lower.ci.ro = mean.ro - qt(1 - (0.05 / 2), n.ro - 1) * se.ro,
+         upper.ci.ro = mean.ro + qt(1 - (0.05 / 2), n.ro - 1) * se.ro)
+avg.multi.tech = multi.tech %>%
+  group_by(model_year, blank_prob, scavenge_prob) %>%
+  summarize(mean.ro = mean(num.rcycl.obj.made),
+            sd.ro = sd(num.rcycl.obj.made),
+            n.ro = n()) %>%
+  mutate(overlap = 2,
+         se.ro = sd.ro / sqrt(n.ro),
+         lower.ci.ro = mean.ro - qt(1 - (0.05 / 2), n.ro - 1) * se.ro,
+         upper.ci.ro = mean.ro + qt(1 - (0.05 / 2), n.ro - 1) * se.ro)
+
+p2 = ggplot() +
+  geom_line(data = avg.two.tech, aes(x = model_year, y = mean.ro, color = as.factor(overlap))) +
+  geom_ribbon(data = avg.two.tech, aes(x = model_year, ymin = lower.ci.ro, ymax = upper.ci.ro), alpha = 0.2) +
+  geom_line(data = avg.multi.tech, aes(x = model_year, y = mean.ro, color = as.factor(overlap))) +
+  geom_ribbon(data = avg.multi.tech, aes(x = model_year, ymin = lower.ci.ro, ymax = upper.ci.ro), alpha = 0.2) +
+  facet_grid(blank_prob ~ scavenge_prob, labeller = labeller(
+    blank_prob = blank.labs, scavenge_prob = scvg.labs
+  )) +
+  scale_x_reverse(breaks = c(500000, 350000, 200000), 
+                  labels = label_number(scale_cut = cut_short_scale())) +
+  scale_y_continuous(labels = label_number(accuracy = 0.01)) +
+  scale_color_colorblind() +
+  labs(color = "overlap parameter", x = "model year", y = "average number of recycled objects created") +
+  theme(strip.text = element_text(size = 5))
+
+save(p1, file = "re-overlap.rdata")
+rm(avg.two.tech, avg.multi.tech)
