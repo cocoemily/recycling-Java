ec50cd4ddbcf24d414ffb45711e7690c03eadfeb
cocoemily <ec3307@nyu.edu>
2023-03-02 13:41:58 -0500

    changes from HPC

--- analysis/ExtendedModel-analysis/experiments/model-analysis/trends-by-blank-scavenging.R
+++ analysis/ExtendedModel-analysis/experiments/model-analysis/trends-by-blank-scavenging.R
@@ -22,6 +22,7 @@ scvg.labs = c("scavenging probability: 0.25", "scavenging probability: 0.50", "s
 names(scvg.labs) = c("0.25", "0.5", "0.75")
 
 load("trends-by-scavenge-blank-probs/ri-overlap.rdata")
+p2 = p1
 load("trends-by-scavenge-blank-probs/re-overlap.rdata")
 load("trends-by-scavenge-blank-probs/ri-mu.rdata")
 load("trends-by-scavenge-blank-probs/re-mu.rdata")
@@ -29,12 +30,12 @@ load("trends-by-scavenge-blank-probs/ri-flake.rdata")
 load("trends-by-scavenge-blank-probs/re-flake.rdata")
 load("trends-by-scavenge-blank-probs/ri-size.rdata")
 load("trends-by-scavenge-blank-probs/re-size.rdata")
-load("trends-by-scavenge-blank-probs/ri-strict.rdata")
-load("trends-by-scavenge-blank-probs/re-strict.rdata")
+#load("trends-by-scavenge-blank-probs/ri-strict.rdata")
+#load("trends-by-scavenge-blank-probs/re-strict.rdata")
 
 
-rgrid = ggarrange(p1, p2, mplot1, mplot2, splot1, splot2, ssplot1, ssplot2, ssp1, ssp2,
-                  nrow = 2, ncol = 5, common.legend = T, 
+rgrid = ggarrange(p2, p1, mplot1, mplot2, splot1, splot2, ssplot1, ssplot2,
+                  nrow = 2, ncol = 4, common.legend = T, 
                   legend = "bottom", labels = "AUTO")
-ggsave(filename = "recycling-trends-by-probs.tiff", grid,
+ggsave(filename = "recycling-trends-by-probs.tiff", rgrid,
        dpi = 300, width = 12, height = 18)
