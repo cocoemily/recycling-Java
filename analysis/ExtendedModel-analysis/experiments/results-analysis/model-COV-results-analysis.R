f1e7207d867c3a79db3631877da5320290d28722
cocoemily <ec3307@nyu.edu>
2023-08-03 15:21:10 -0400

    create model COV figures on HPC

--- analysis/ExtendedModel-analysis/experiments/results-analysis/model-COV-results-analysis.R
+++ analysis/ExtendedModel-analysis/experiments/results-analysis/model-COV-results-analysis.R
@@ -5,72 +5,70 @@ library(scales)
 
 theme_set(theme_bw())
 
-parameters = c("max_use_intensity", "max_artifact_carry", "max_flake_size","max_nodules_size", "blank_prob", "scavenge_prob", "overlap","mu", "size_preference", "flake_preference","min_suitable_flake_size", "strict_selection")
-model_var = read_csv("~/eclipse-workspace/recycling-Java/results/model-variation-output/all-model-COV.csv")
+parameters = c("max_use_intensity", "max_artifact_carry", "max_flake_size","max_nodules_size", "blank_prob", "scavenge_prob", "overlap","mu", "num_agents", "size_preference", "flake_preference","min_suitable_flake_size", "strict_selection")
+model_var = read_csv("~/eclipse-workspace/recycling-Java/results/model-coefficients-of-variation.csv")
 #model_var = read_csv("/scratch/ec3307/recycling-Java/results/all-model-variation.csv")
 
-rcycl.obj = model_var[,c(parameters, "num.rcycl.obj.made", "model_year")]
-blank.events = model_var[,c(parameters, "num.blank.events", "model_year")]
-retouch.events = model_var[,c(parameters, "num.retouch.events", "model_year")]
-discard.events = model_var[,c(parameters, "num.discard.events", "model_year")]
-scavenge.events = model_var[,c(parameters, "num.scav.events", "model_year")]
+rcycl.obj = model_var[,c(parameters, "COV.num.rcycl.obj.made", "model_year")]
+blank.events = model_var[,c(parameters, "COV.num.blank.events", "model_year")]
+retouch.events = model_var[,c(parameters, "COV.num.retouch.events", "model_year")]
+discard.events = model_var[,c(parameters, "COV.num.discard.events", "model_year")]
+scavenge.events = model_var[,c(parameters, "COV.num.scav.events", "model_year")]
 
 #rm(model_var)
 
 #### Variation of recycled objects made ####
 r1 = ggplot(rcycl.obj) +
-  geom_smooth(aes(x = model_year, y = num.rcycl.obj.made, color = as.factor(mu), group = as.factor(mu))) +
-  scale_x_reverse(breaks = c(500000, 350000, 200000), 
-                  labels = label_number(scale_cut = cut_short_scale())) +
+  geom_smooth(aes(x = model_year, y = COV.num.rcycl.obj.made, color = as.factor(mu), group = as.factor(mu))) +
+  facet_wrap(~ num_agents, labeller = label_both)+
+  scale_x_reverse(labels = label_number(scale_cut = cut_short_scale())) +
   scale_color_colorblind() +
-  labs(color = "mu", x = "model year", y = "variance of number of recycled objects made")
+  labs(color = "mu", x = "model year", y = "COV of number of recycled objects made")
 #ggsave(filename = "rcycl-obj-var_mu.png", plot=r1, dpi = 300)
 
 r2 = ggplot(rcycl.obj) +
-  geom_smooth(aes(x = model_year, y = num.rcycl.obj.made, color = as.factor(overlap), group = as.factor(overlap))) +
-  scale_x_reverse(breaks = c(500000, 350000, 200000), 
-                  labels = label_number(scale_cut = cut_short_scale())) +
+  geom_smooth(aes(x = model_year, y = COV.num.rcycl.obj.made, color = as.factor(overlap), group = as.factor(overlap))) +
+  scale_x_reverse(labels = label_number(scale_cut = cut_short_scale())) +
   scale_color_colorblind() +
-  labs(color = "overlap", x = "model year", y = "variance of number of recycled objects made")
+  labs(color = "overlap", x = "model year", y = "COV of number of recycled objects made")
 #ggsave(filename = "rcycl-obj-var_overlap.png", plot=r2, dpi = 300)
 
 r3 = ggplot(rcycl.obj) +
-  geom_smooth(aes(x = model_year, y = num.rcycl.obj.made, color = as.factor(flake_preference), group = as.factor(flake_preference))) +
+  geom_smooth(aes(x = model_year, y = COV.num.rcycl.obj.made, color = as.factor(flake_preference), group = as.factor(flake_preference))) +
   facet_grid(size_preference ~ strict_selection, labeller = label_both)+
-  scale_x_reverse(breaks = c(500000, 350000, 200000), 
-                  labels = label_number(scale_cut = cut_short_scale())) +
+  scale_x_reverse(labels = label_number(scale_cut = cut_short_scale())) +
   scale_color_colorblind() +
-  labs(color = "flake preference", x = "model year", y = "variance of number of recycled objects made")
+  labs(color = "flake preference", x = "model year", y = "COV of number of recycled objects made")
 #ggsave(filename = "rcycl-obj-var_selection.png", plot=r3, dpi = 300)
 
+rplot = ggarrange(r1, r2, r3, ncol = 3, nrow = 1, legend = "bottom", labels = "AUTO")
+plot(rplot)
 ggsave(filename = "../results/model-variation-output/rcycl-obj-var.tiff",
-  ggarrange(r1, r2, r3, ncol = 3, nrow = 1, legend = "bottom", labels = "AUTO"), 
+  rplot, 
   dpi = 300, width = 10, height = 6)
 
 #### Variation of blank creation events ####
 b1 = ggplot(blank.events) +
-  geom_smooth(aes(x = model_year, y = num.blank.events, color = as.factor(mu), group = as.factor(mu))) +
-  scale_x_reverse(breaks = c(500000, 350000, 200000), 
-                  labels = label_number(scale_cut = cut_short_scale())) +
+  geom_smooth(aes(x = model_year, y = COV.num.blank.events, color = as.factor(mu), group = as.factor(mu))) +
+  facet_wrap(~ num_agents, labeller = label_both)+
+  scale_x_reverse(labels = label_number(scale_cut = cut_short_scale())) +
   scale_color_colorblind() +
-  labs(color = "mu", x = "model year", y = "variance of number of blank events")
+  labs(color = "mu", x = "model year", y = "COV of number of blank events")
 #ggsave(filename = "blank-events-var_mu.png", plot=b1, dpi = 300)
 
 b2 = ggplot(blank.events) +
-  geom_smooth(aes(x = model_year, y = num.blank.events, color = as.factor(overlap), group = as.factor(overlap))) +
-  scale_x_reverse(breaks = c(500000, 350000, 200000), 
-                  labels = label_number(scale_cut = cut_short_scale())) +
+  geom_smooth(aes(x = model_year, y = COV.num.blank.events, color = as.factor(overlap), group = as.factor(overlap))) +
+  scale_x_reverse(labels = label_number(scale_cut = cut_short_scale())) +
   scale_color_colorblind() +
-  labs(color = "overlap", x = "model year", y = "variance of number of blank events")
+  labs(color = "overlap", x = "model year", y = "COV of number of blank events")
 #ggsave(filename = "blank-events-var_overlap.png", plot=b2, dpi = 300)
 
 b3 = ggplot(blank.events) +
-  geom_smooth(aes(x = model_year, y = num.blank.events, color = as.factor(flake_preference), group = as.factor(flake_preference))) +
+  geom_smooth(aes(x = model_year, y = COV.num.blank.events, color = as.factor(flake_preference), group = as.factor(flake_preference))) +
   facet_grid(size_preference ~ strict_selection, labeller = label_both) +
-  scale_x_reverse(breaks = c(500000, 350000, 200000), 
-                  labels = label_number(scale_cut = cut_short_scale())) +
+  scale_x_reverse(labels = label_number(scale_cut = cut_short_scale())) +
   scale_color_colorblind()  +
-  labs(color = "flake preference", x = "model year", y = "variance of number of blank events")
+  labs(color = "flake preference", x = "model year", y = "COV of number of blank events")
 #ggsave(filename = "blank-events-var_selection.png", plot=b3, dpi = 300)
 
 ggsave(filename = "../results/model-variation-output/blank-events-var.tiff",
@@ -79,28 +77,26 @@ ggsave(filename = "../results/model-variation-output/blank-events-var.tiff",
 
 #### Variation of retouch events ####
 rt1 = ggplot(retouch.events) +
-  geom_smooth(aes(x = model_year, y = num.retouch.events, color = as.factor(mu), group = as.factor(mu))) +
-  scale_x_reverse(breaks = c(500000, 350000, 200000), 
-                  labels = label_number(scale_cut = cut_short_scale())) +
+  geom_smooth(aes(x = model_year, y = COV.num.retouch.events, color = as.factor(mu), group = as.factor(mu))) +
+  facet_wrap(~ num_agents, labeller = label_both)+
+  scale_x_reverse(labels = label_number(scale_cut = cut_short_scale())) +
   scale_color_colorblind()  +
-  labs(color = "mu", x = "model year", y = "variance of number of retouch events")
+  labs(color = "mu", x = "model year", y = "COV of number of retouch events")
 #ggsave(filename = "retouch-events-var_mu.png", plot=rt1, dpi = 300)
 
 rt2 = ggplot(retouch.events) +
-  geom_smooth(aes(x = model_year, y = num.retouch.events, color = as.factor(overlap), group = as.factor(overlap))) +
-  scale_x_reverse(breaks = c(500000, 350000, 200000), 
-                  labels = label_number(scale_cut = cut_short_scale())) +
+  geom_smooth(aes(x = model_year, y = COV.num.retouch.events, color = as.factor(overlap), group = as.factor(overlap))) +
+  scale_x_reverse(labels = label_number(scale_cut = cut_short_scale())) +
   scale_color_colorblind() +
-  labs(color = "overlap", x = "model year", y = "variance of number of retouch events")
+  labs(color = "overlap", x = "model year", y = "COV of number of retouch events")
 #ggsave(filename = "retouch-events-var_overlap.png", plot=rt2, dpi = 300)
 
 rt3 = ggplot(retouch.events) +
-  geom_smooth(aes(x = model_year, y = num.retouch.events, color = as.factor(flake_preference), group = as.factor(flake_preference))) +
+  geom_smooth(aes(x = model_year, y = COV.num.retouch.events, color = as.factor(flake_preference), group = as.factor(flake_preference))) +
   facet_grid(size_preference ~ strict_selection, labeller = label_both) +
-  scale_x_reverse(breaks = c(500000, 350000, 200000), 
-                  labels = label_number(scale_cut = cut_short_scale())) +
+  scale_x_reverse(labels = label_number(scale_cut = cut_short_scale())) +
   scale_color_colorblind() +
-  labs(color = "flake preference", x = "model year", y = "variance of number of retouch events")
+  labs(color = "flake preference", x = "model year", y = "COV of number of retouch events")
 #ggsave(filename = "retouch-events-var_selection.png", plot=rt3, dpi = 300)
 
 ggsave(filename = "../results/model-variation-output/retouch-events-var.tiff",
@@ -109,28 +105,26 @@ ggsave(filename = "../results/model-variation-output/retouch-events-var.tiff",
 
 #### Variation of discard events ####
 d1 = ggplot(discard.events) +
-  geom_smooth(aes(x = model_year, y = num.discard.events, color = as.factor(mu), group = as.factor(mu))) +
-  scale_x_reverse(breaks = c(500000, 350000, 200000), 
-                  labels = label_number(scale_cut = cut_short_scale())) +
+  geom_smooth(aes(x = model_year, y = COV.num.discard.events, color = as.factor(mu), group = as.factor(mu))) +
+  facet_wrap(~ num_agents, labeller = label_both)+
+  scale_x_reverse(labels = label_number(scale_cut = cut_short_scale())) +
   scale_color_colorblind()  +
-  labs(color = "mu", x = "model year", y = "variance of number of discard events")
+  labs(color = "mu", x = "model year", y = "COV of number of discard events")
 #ggsave(filename = "discard-events-var_mu.png", plot=d1, dpi = 300)
 
 d2 = ggplot(discard.events) +
-  geom_smooth(aes(x = model_year, y = num.discard.events, color = as.factor(overlap), group = as.factor(overlap))) +
-  scale_x_reverse(breaks = c(500000, 350000, 200000), 
-                  labels = label_number(scale_cut = cut_short_scale())) +
+  geom_smooth(aes(x = model_year, y = COV.num.discard.events, color = as.factor(overlap), group = as.factor(overlap))) +
+  scale_x_reverse(labels = label_number(scale_cut = cut_short_scale())) +
   scale_color_colorblind() +
-  labs(color = "overlap", x = "model year", y = "variance of number of discard events")
+  labs(color = "overlap", x = "model year", y = "COV of number of discard events")
 #ggsave(filename = "discard-events-var_overlap.png", plot=d2, dpi = 300)
 
 d3 = ggplot(discard.events) +
-  geom_smooth(aes(x = model_year, y = num.discard.events, color = as.factor(flake_preference), group = as.factor(flake_preference))) +
+  geom_smooth(aes(x = model_year, y = COV.num.discard.events, color = as.factor(flake_preference), group = as.factor(flake_preference))) +
   facet_grid(size_preference ~ strict_selection, labeller = label_both) +
-  scale_x_reverse(breaks = c(500000, 350000, 200000), 
-                  labels = label_number(scale_cut = cut_short_scale())) +
+  scale_x_reverse(labels = label_number(scale_cut = cut_short_scale())) +
   scale_color_colorblind() +
-  labs(color = "flake preference", x = "model year", y = "variance of number of discard events")
+  labs(color = "flake preference", x = "model year", y = "COV of number of discard events")
 #ggsave(filename = "discard-events-var_selection.png", plot=d3, dpi = 300)
 
 ggsave(filename = "../results/model-variation-output/discard-events-var.tiff",
@@ -139,28 +133,26 @@ ggsave(filename = "../results/model-variation-output/discard-events-var.tiff",
 
 #### Variation of scavenging events ####
 s1 = ggplot(scavenge.events) +
-  geom_smooth(aes(x = model_year, y = num.scav.events, color = as.factor(mu), group = as.factor(mu))) +
-  scale_x_reverse(breaks = c(500000, 350000, 200000), 
-                  labels = label_number(scale_cut = cut_short_scale())) +
+  geom_smooth(aes(x = model_year, y = COV.num.scav.events, color = as.factor(mu), group = as.factor(mu))) +
+  facet_wrap(~ num_agents, labeller = label_both)+
+  scale_x_reverse(labels = label_number(scale_cut = cut_short_scale())) +
   scale_color_colorblind()  +
-  labs(color = "mu", x = "model year", y = "variance of number of scavenging events")
+  labs(color = "mu", x = "model year", y = "COV of number of scavenging events")
 #ggsave(filename = "scavenge-events-var_mu.png", plot=s1, dpi = 300)
 
 s2 = ggplot(scavenge.events) +
-  geom_smooth(aes(x = model_year, y = num.scav.events, color = as.factor(overlap), group = as.factor(overlap))) +
-  scale_x_reverse(breaks = c(500000, 350000, 200000), 
-                  labels = label_number(scale_cut = cut_short_scale())) +
+  geom_smooth(aes(x = model_year, y = COV.num.scav.events, color = as.factor(overlap), group = as.factor(overlap))) +
+  scale_x_reverse(labels = label_number(scale_cut = cut_short_scale())) +
   scale_color_colorblind() +
-  labs(color = "overlap", x = "model year", y = "variance of number of scavenging events")
+  labs(color = "overlap", x = "model year", y = "COV of number of scavenging events")
 #ggsave(filename = "scavenge-events-var_overlap.png", plot=s2, dpi = 300)
 
 s3 = ggplot(scavenge.events) +
-  geom_smooth(aes(x = model_year, y = num.scav.events, color = as.factor(flake_preference), group = as.factor(flake_preference))) +
+  geom_smooth(aes(x = model_year, y = COV.num.scav.events, color = as.factor(flake_preference), group = as.factor(flake_preference))) +
   facet_grid(size_preference ~ strict_selection, labeller = label_both) +
-  scale_x_reverse(breaks = c(500000, 350000, 200000), 
-                  labels = label_number(scale_cut = cut_short_scale())) +
+  scale_x_reverse(labels = label_number(scale_cut = cut_short_scale())) +
   scale_color_colorblind() +
-  labs(color = "flake preference", x = "model year", y = "variance of number of scavenging events")
+  labs(color = "flake preference", x = "model year", y = "COV of number of scavenging events")
 #ggsave(filename = "scavenge-events-var_selection.png", plot=d3, dpi = 300)
 
 ggsave(filename = "../results/model-variation-output/scavenge-events-var.tiff",
